8aba103ea28695b9422da367b78e3fff26a8710c
Tobias Linn <tl@ithe.rwth-aachen.de>
2019-12-02 16:08:02 +0100

    split matrix in multiple files; eye for dense, sparse and band matrices

---
+++ src/util/matrix/matrix.f90
@@ -0,0 +1,70 @@
+#include "../assert.f90.inc"
+#include "../macro.f90.inc"
+
+module matrix_m
+  use array_m
+  use blas95
+  use lapack95
+  use omp_lib
+  use pardiso_m
+  use qsort_m
+  use vector_m
+  implicit none
+
+#define T real
+#define TT real
+#include "matrix_def.f90.inc"
+
+#define T cmplx
+#define TT complex
+#define TCMPLX
+#include "matrix_def.f90.inc"
+
+contains
+
+#define T real
+#define TT real
+#include "matrix_imp.f90.inc"
+
+#define T real
+#define TT real
+#include "dense_imp.f90.inc"
+
+#define T real
+#define TT real
+#include "sparse_imp.f90.inc"
+
+#define T real
+#define TT real
+#include "band_imp.f90.inc"
+
+#define T real
+#define TT real
+#include "block_imp.f90.inc"
+
+#define T cmplx
+#define TT complex
+#define TCMPLX
+#include "matrix_imp.f90.inc"
+
+#define T cmplx
+#define TT complex
+#define TCMPLX
+#include "dense_imp.f90.inc"
+
+#define T cmplx
+#define TT complex
+#define TCMPLX
+#include "sparse_imp.f90.inc"
+
+#define T cmplx
+#define TT complex
+#define TCMPLX
+#include "band_imp.f90.inc"
+
+#define T cmplx
+#define TT complex
+#define TCMPLX
+#include "block_imp.f90.inc"
+
+end module
