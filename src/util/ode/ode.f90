fb397361375812d2220aad395f34135741060b45
Tobias Linn <tl@ithe.rwth-aachen.de>
2020-01-22 20:27:56 +0100

    RADAU5 ode solver

---
+++ src/util/ode/ode.f90
@@ -0,0 +1,300 @@
+module ode_m
+  use error_m
+  use high_precision_m
+  implicit none
+
+  type ode_options
+    !! options for ode solver
+    real, allocatable :: atol(:)
+      !! absolute tolerance for each component
+    real, allocatable :: rtol(:)
+      !! relative tolerance for each component
+    real              :: newton_atol
+      !! absolute tolerance for newton iteration
+    real              :: newton_rtol
+      !! relative tolerance for newton iteration
+    integer           :: newton_max_it
+      !! maximum number of allowed newton iterations
+    integer           :: max_rejected
+      !! maximum number of allowed rejected steps
+    real              :: min_rx
+      !! minimum relative stepsize
+  contains
+    procedure :: init => ode_options_init ! initialize
+  end type
+
+  interface
+    subroutine ode_fun(x, U, P, f, dfdU, dfdP)
+      real,              intent(in)  :: x
+        !! x coordinate
+      real,              intent(in)  :: U(:)
+        !! state (nU)
+      real,              intent(in)  :: P(:)
+        !! parameters (nP)
+      real, optional,    intent(out) :: f(:)
+        !! output dU/dx (nU)
+      real, optional,    intent(out) :: dfdU(:,:)
+        !! output derivatives of f wrt U (nU,nU)
+      real, optional,    intent(out) :: dfdP(:,:)
+        !! output derivatives of f wrt P (nU,nP)
+    end subroutine
+
+    subroutine ode_kernel(fun, xold, x, dxk, Uk, dUkdQ, fk, dfkdUk, polyk, &
+      P, opt, dxn, Un, dUndQ, fn, dfndUn, polyn, dpolyndQ, err, status)
+      import
+      procedure(ode_fun), pointer, intent(in)    :: fun
+        !! function to integrate
+      real,                        intent(in)    :: xold
+        !! initial x position of previous step
+      real,                        intent(in)    :: x
+        !! initial x position of step
+      real,                        intent(in)    :: dxk
+        !! stepsize
+      real,                        intent(in)    :: Uk(:)
+        !! state at beginning of step
+      real,                        intent(in)    :: dUkdQ(:,:)
+        !! derivatives of Uk wrt Q = [U0; P]
+      real,                        intent(in)    :: fk(:)
+        !! dUk/dx
+      real,                        intent(in)    :: dfkdUk(:,:)
+        !! derivatives of fk wrt Uk
+      real,                        intent(in)    :: polyk(:,:)
+        !! interpolation polynomial (from previous step)
+      real,                        intent(in)    :: P(:)
+        !! parameters
+      type(ode_options),           intent(in)    :: opt
+        !! options
+      real,                        intent(out)   :: dxn
+        !! output new stepsize
+      real,                        intent(out)   :: Un(:)
+        !! output state at end of step
+      real,                        intent(out)   :: dUndQ(:,:)
+        !! output derivatives of Un wrt Q = [U0; P]
+      real,                        intent(out)   :: fn(:)
+        !! output dUn/dx
+      real,                        intent(out)   :: dfndUn(:,:)
+        !! output derivatives of fn wrt Un
+      real,                        intent(out)   :: polyn(:,:)
+        !! output interpolation polynomial for this step
+      real,                        intent(out)   :: dpolyndQ(:,:,:)
+        !! output derivatives of polyn wrt Q = [U0; P]
+      real,                        intent(inout) :: err
+        !! output scalar error estimate
+      logical,                     intent(inout) :: status
+        !! output success (true) or fail (false)
+    end subroutine
+  end interface
+
+contains
+
+  subroutine ode_solve(kernel, nS, fun, x0, x1, U0, P, opt, U1, dU1dU0, dU1dP, UA, dUAdU0, dUAdP, nsteps)
+    procedure(ode_kernel), pointer, intent(in)  :: kernel
+      !! pointer to ode solver kernel
+    integer,                        intent(in)  :: nS
+      !! number of stages of kernel
+    procedure(ode_fun),    pointer, intent(in)  :: fun
+      !! pointer to function to integrate
+    real,                           intent(in)  :: x0
+      !! initial x
+    real,                           intent(in)  :: x1
+      !! final x
+    real,                           intent(in)  :: U0(:)
+      !! initial state
+    real,                           intent(in)  :: P(:)
+      !! parameters
+    type(ode_options),              intent(in)  :: opt
+      !! solver options
+    real,                           intent(out) :: U1(:)
+      !! output final state
+    real,    optional,              intent(out) :: dU1dU0(:,:)
+      !! optional output derivatives of U1 wrt U0
+    real,    optional,              intent(out) :: dU1dP(:,:)
+      !! optional output derivatives of U1 wrt P
+    real,    optional,              intent(out) :: UA(:)
+      !! optional output average state in interval [x0, x1]
+    real,    optional,              intent(out) :: dUAdU0(:,:)
+      !! optional output derivatives of UA wrt U0
+    real,    optional,              intent(out) :: dUAdP(:,:)
+      !! optional output derivatives of UA wrt P
+    integer, optional,              intent(out) :: nsteps
+      !! optional output number of steps taken
+
+    ! local variables
+    integer :: nU, nP
+
+    ! get system size and number of parameters
+    nU = size(U0)
+    nP = size(P)
+
+    block
+      integer       :: i, j, rejection_counter, nsteps_
+      real          :: x, xold, dxk, dxn, err
+      real          :: Uk(nU), dUkdQ(nU,nU+nP), fk(nU), dfkdUk(nU,nU), polyk(nU,0:nS)
+      real          :: Un(nU), dUndQ(nU,nU+nP), fn(nU), dfndUn(nU,nU), polyn(nU,0:nS), dpolyndQ(nU,nU+nP,nS)
+      type(hp_real) :: hp_UA(nU), hp_dUAdQ(nU,nU+nP)
+      logical       :: status, avg
+
+      ! average state wanted?
+      avg = .false.
+      if (present(UA) .or. present(dUAdU0) .or. present(dUAdP)) avg = .true.
+
+      ! initial x and dx
+      x    = x0
+      xold = x0
+      dxk = (x1 - x0) / 8
+
+      ! reset step and rejection counter
+      nsteps_ = 0
+      rejection_counter = 0
+
+      ! initial state
+      Uk    = U0
+      dUkdQ = 0
+      do i = 1, nU
+        dUkdQ(i,i) = 1.0
+      end do
+
+      ! eval f and derivatives at U0
+      call fun(x0, U0, P, f = fk, dfdU = dfkdUk)
+
+      ! init interpolation polynomial (linear extrapolation)
+      polyk      = 0
+      polyk(:,1) = fk
+
+      ! init average state
+      if (avg) then
+        hp_UA    = real_to_hp(0.0)
+        hp_dUAdQ = real_to_hp(0.0)
+      end if
+
+      ! initial values for error control
+      status = .false.
+      err    = 1e99
+
+      ! main loop
+      do while (abs(x - x0) < abs(x1 - x0))
+        ! kernel
+        call kernel(fun, xold, x, dxk, Uk, dUkdQ, fk, dfkdUk, polyk, P, opt, &
+                                  dxn, Un, dUndQ, fn, dfndUn, polyn, dpolyndQ, err, status)
+
+        if (.not. status) then
+          ! reject step
+          rejection_counter = rejection_counter + 1
+          if (rejection_counter > opt%max_rejected) then
+            print "(1A, 1E64.56)", "x0 = ", x0
+            print "(1A, 1E64.56)", "x1 = ", x1
+            do i = 1, nU
+              print "(1A, 1I1, 1A, 1E64.56)", "U0(", i, ") = ", U0(i)
+            end do
+            do i = 1, nP
+              print "(1A, 1I1, 1A, 1E64.56)", "P(", i, ") = ", P(i)
+            end do
+
+            call program_error("Can not solve ode, limit for rejected steps reached!")
+          end if
+        else
+          ! accept step, reset rejection counter
+          rejection_counter = 0
+
+          ! update average state by integrating the interpolation polynomial
+          if (avg) then
+            hp_UA    = hp_UA    + dxk * Uk
+            hp_dUAdQ = hp_dUAdQ + dxk * dUkdQ
+            do j = 1, nS
+              hp_UA    = hp_UA    + dxk**(j+1) / real(j+1) * polyn(:,j)
+              hp_dUAdQ = hp_dUAdQ + dxk**(j+1) / real(j+1) * dpolyndQ(:,:,j)
+            end do
+          end if
+
+          ! advance x
+          xold = x
+          x    = x + dxk
+
+          ! copy n state to k state
+          Uk     = Un
+          dUkdQ  = dUndQ
+          fk     = fn
+          dfkdUk = dfndUn
+          polyk  = polyn
+
+          ! update step counter
+          nsteps_ = nsteps_ + 1
+        end if
+
+        ! adjust step size
+        dxk = dxn
+        if (abs(x + dxk - x0) > abs(x1 - x0)) dxk = x1 - x
+      end do
+
+      ! set end state and extract derivatives from dUkdQ
+      U1 = Uk
+      if (present(dU1dU0)) dU1dU0 = dUkdQ(:,1:nU)
+      if (present(dU1dP )) dU1dP  = dUkdQ(:,(nU+1):(nU+nP))
+
+      ! get average state
+      if (present(UA    )) UA     = hp_to_real(hp_UA                     ) / (x1 - x0)
+      if (present(dUAdU0)) dUAdU0 = hp_to_real(hp_dUAdQ(:,1:nU)          ) / (x1 - x0)
+      if (present(dUAdP )) dUAdP  = hp_to_real(hp_dUAdQ(:,(nU+1):(nU+nP))) / (x1 - x0)
+
+      ! output number of steps
+      if (present(nsteps)) nsteps = nsteps_
+    end block
+  end subroutine
+
+  subroutine ode_options_init(this, nU, atol, rtol, newton_atol, newton_rtol, newton_max_it, max_rejected, min_rx)
+    !! initialize ode solver options
+    class(ode_options), intent(out) :: this
+    integer,            intent(in)  :: nU
+      !! system size
+    real,    optional,  intent(in)  :: atol(:)
+      !! absolute tolerance for each component (default: 1e-16)
+    real,    optional,  intent(in)  :: rtol(:)
+      !! relative tolerance for each component (default: 1e-12)
+    real,    optional,  intent(in)  :: newton_atol
+      !! absolute tolerance for newton iteration (default: 1e-16)
+    real,    optional,  intent(in)  :: newton_rtol
+      !! relative tolerance for newton iteration (default: 1e-14)
+    integer, optional,  intent(in)  :: newton_max_it
+      !! maximum number of allowed newton iterations (default: 5)
+    integer, optional,  intent(in)  :: max_rejected
+      !! maximum number of allowed rejected steps (default: 10)
+    real,    optional,  intent(in)  :: min_rx
+      !! minimum relative stepsize (default: 1e-8)
+
+    allocate (this%atol(nU), source = 1e-16)
+    allocate (this%rtol(nU), source = 1e-12)
+
+    if (present(atol)) then
+      this%atol = atol
+    end if
+    if (present(rtol)) then
+      this%rtol = rtol
+    end if
+    if (present(newton_atol)) then
+      this%newton_atol = newton_atol
+    else
+      this%newton_atol = 1e-16
+    end if
+    if (present(newton_rtol)) then
+      this%newton_rtol = newton_rtol
+    else
+      this%newton_rtol = 1e-14
+    end if
+    if (present(newton_max_it)) then
+      this%newton_max_it = newton_max_it
+    else
+      this%newton_max_it = 5
+    end if
+    if (present(max_rejected)) then
+      this%max_rejected = max_rejected
+    else
+      this%max_rejected = 10
+    end if
+    if (present(min_rx)) then
+      this%min_rx = min_rx
+    else
+      this%min_rx = 1e-8
+    end if
+  end subroutine
+
+end module
---
+++ src/util/ode/radau5.f90
@@ -0,0 +1,498 @@
+module radau5_m
+  use lapack95
+  use ode_m
+  implicit none
+
+  private
+  public :: radau5
+
+  ! radau5 parameters
+  real, parameter :: C(3) = [ &
+    (4.0 - sqrt(6.0)) / 10.0, &
+    (4.0 + sqrt(6.0)) / 10.0, &
+    1.0                       &
+  ]
+  real, parameter :: A(3,3) = reshape([    &
+    (  88.0 -   7.0 * sqrt(6.0)) /  360.0, &
+    ( 296.0 + 169.0 * sqrt(6.0)) / 1800.0, &
+    (  16.0 -         sqrt(6.0)) /   36.0, &
+    ( 296.0 - 169.0 * sqrt(6.0)) / 1800.0, &
+    (  88.0 +   7.0 * sqrt(6.0)) /  360.0, &
+    (  16.0 +         sqrt(6.0)) /   36.0, &
+    (-  2.0 +   3.0 * sqrt(6.0)) /  225.0, &
+    (-  2.0 -   3.0 * sqrt(6.0)) /  225.0, &
+    (   1.0                    ) /    9.0  &
+  ], [ 3, 3 ])
+  real, parameter :: G0   = 2.74888829595676675854321047154372d-01
+  real, parameter :: E(3) = [             &
+      G0 * (-13.0 - 7.0 * sqrt(6.0)) / 3.0, &
+      G0 * (-13.0 + 7.0 * sqrt(6.0)) / 3.0, &
+      G0 * (- 1.0                  ) / 3.0  &
+  ]
+
+contains
+
+  subroutine radau5(fun, x0, x1, U0, P, opt, U1, dU1dU0, dU1dP, UA, dUAdU0, dUAdP, nsteps)
+    !! radau5 ode solver
+    procedure(ode_fun),    pointer, intent(in)  :: fun
+      !! pointer to function to integrate
+    real,                           intent(in)  :: x0
+      !! initial x
+    real,                           intent(in)  :: x1
+      !! final x
+    real,                           intent(in)  :: U0(:)
+      !! initial state
+    real,                           intent(in)  :: P(:)
+      !! parameters
+    type(ode_options),              intent(in)  :: opt
+      !! solver options
+    real,                           intent(out) :: U1(:)
+      !! output final state
+    real,    optional,              intent(out) :: dU1dU0(:,:)
+      !! optional output derivatives of U1 wrt U0
+    real,    optional,              intent(out) :: dU1dP(:,:)
+      !! optional output derivatives of U1 wrt P
+    real,    optional,              intent(out) :: UA(:)
+      !! optional output average state in interval [x0, x1]
+    real,    optional,              intent(out) :: dUAdU0(:,:)
+      !! optional output derivatives of UA wrt U0
+    real,    optional,              intent(out) :: dUAdP(:,:)
+      !! optional output derivatives of UA wrt P
+    integer, optional,              intent(out) :: nsteps
+      !! optional output number of steps taken
+
+    ! call base solver with radau5 kernel (3 stages)
+    call ode_solve(radau5_kernel, 3, fun, x0, x1, U0, P, opt, U1, dU1dU0, dU1dP, UA, dUAdU0, dUAdP, nsteps)
+  end subroutine
+
+  subroutine radau5_kernel(fun, xold, x, dxk, Uk, dUkdQ, fk, dfkdUk, polyk, P, opt, dxn, Un, dUndQ, fn, dfndUn, polyn, dpolyndQ, err, status)
+    !! kernel for radau5 ode solver
+    procedure(ode_fun), pointer, intent(in)    :: fun
+      !! function to integrate
+    real,                        intent(in)    :: xold
+      !! initial x position of previous step
+    real,                        intent(in)    :: x
+      !! initial x position of step
+    real,                        intent(in)    :: dxk
+      !! stepsize
+    real,                        intent(in)    :: Uk(:)
+      !! state at beginning of step
+    real,                        intent(in)    :: dUkdQ(:,:)
+      !! derivatives of Uk wrt Q = [U0; P]
+    real,                        intent(in)    :: fk(:)
+      !! dUk/dx
+    real,                        intent(in)    :: dfkdUk(:,:)
+      !! derivatives of fk wrt Uk
+    real,                        intent(in)    :: polyk(:,:)
+      !! interpolation polynomial (from previous step)
+    real,                        intent(in)    :: P(:)
+      !! parameters
+    type(ode_options),           intent(in)    :: opt
+      !! options
+    real,                        intent(out)   :: dxn
+      !! output new stepsize
+    real,                        intent(out)   :: Un(:)
+      !! output state at end of step
+    real,                        intent(out)   :: dUndQ(:,:)
+      !! output derivatives of Un wrt Q = [U0; P]
+    real,                        intent(out)   :: fn(:)
+      !! output dUn/dx
+    real,                        intent(out)   :: dfndUn(:,:)
+      !! output derivatives of fn wrt Un
+    real,                        intent(out)   :: polyn(:,:)
+      !! output interpolation polynomial for this step
+    real,                        intent(out)   :: dpolyndQ(:,:,:)
+      !! output derivatives of polyn wrt Q = [U0; P]
+    real,                        intent(inout) :: err
+      !! output scalar error estimate
+    logical,                     intent(inout) :: status
+      !! output success (true) or fail (false)
+
+    ! local variables
+    integer :: nU, nP
+
+    ! get system size, number of parameters and number of stages
+    nU = size(Uk)
+    nP = size(P)
+
+    block
+      integer :: it, i, j, i0, i1
+      real    :: newt_err, newt_err0, err_k
+      real    :: f(nU,3), dfdz(nU,nU,3), dfdP(nU,nP,3)
+      real    :: z(nU*3), h(nU*3,1), dhdz(nU*3,nU*3)
+      real    :: dhdQ(nU*3,nu+nP), dzdQ(nu*3,nu+nP)
+
+      ! reset output
+      dxn      = 0
+      Un       = 0
+      dUndQ    = 0
+      fn       = 0
+      dfndUn   = 0
+      polyn    = 0
+      dpolyndQ = 0
+
+      ! initial guess for z (use interpolation polynomial from last step)
+      call initial_guess(nU, xold, x, dxk, polyk, z)
+
+      ! newton iteration
+      newt_err0 = 2e99
+      newt_err  = 1e99
+      it = 0
+      do while (newt_err > 0)
+        ! count iterations
+        it = it + 1
+
+        ! fail if too many iterations, or error grows
+        if ((it > opt%newton_max_it) .or. (newt_err > newt_err0)) then
+          ! try half step size next time
+          dxn = 0.5 * dxk
+
+          ! fail
+          status = .false.
+          return
+        end if
+
+        ! evaluate function and derivatives at all stages
+        call eval_f(nU, fun, x, dxk, Uk, z, P, f, dfdz)
+
+        ! get h and dhdz
+        call eval_h(nU, dxk, f, dfdz, z, h(:,1), dhdz)
+
+        ! solve
+        call gesv(dhdz, h)
+
+        ! get error
+        newt_err0 = newt_err
+        newt_err  = maxval(abs(h(:,1)) - max(opt%newton_rtol * abs(z), opt%newton_atol))
+
+        ! update z
+        z = z - h(:,1)
+      end do
+
+      ! get U at beginning of next step
+      Un = Uk + z((2*nU+1):(3*nU))
+
+      ! get error estimate
+      call error_estimate(nU, fun, x, dxk, Uk, fk, dfkdUk, P, opt, z, Un, status, err_k)
+
+      ! step size prediction
+      call step_size_prediction(x, x - xold, dxk, err, err_k, opt, it, dxn)
+
+      ! fail if err > 1
+      if (err_k > 1.0) then
+        status = .false.
+      else
+        ! success
+        status = .true.
+        err    = err_k
+
+        ! derivatives
+        call eval_f(nU, fun, x, dxk, Uk, z, P, f, dfdz, dfdP = dfdP)
+        call eval_h(nU, dxk, f, dfdz, z, h(:,1), dhdz)
+        call eval_dhdQ(nU, nP, dxk, dfdz, dfdP, dUkdQ, dhdQ)
+
+        ! calculate dzdQ = - (dhdz)^(-1) * dhdQ
+        call gesv(dhdz, dhdQ)
+        dzdQ = - dhdQ
+
+        ! get dUndQ
+        dUndQ = dUkdQ + dzdQ((nU*2+1):(nU*3),:)
+
+        ! set fn and dfndUn
+        fn     = f(:,3)
+        dfndUn = dfdz(:,:,3)
+
+        ! new polynomial interpolation
+        call interpolate(dxk, z(1:nU), z((nU+1):(2*nU)), z((2*nU+1):(3*nU)), &
+                         dzdQ(1:nU,:), dzdQ((nU+1):(2*nU),:), dzdQ((2*nU+1):(3*nU),:), polyn, dpolyndQ)
+      end if
+    end block
+  end subroutine
+
+  subroutine initial_guess(nU, xold, x, dxk, polyk, z)
+    integer, intent(in)  :: nU
+      !! system size
+    real,    intent(in)  :: xold
+      !! initial x position of previous step
+    real,    intent(in)  :: x
+      !! initial x position of step
+    real,    intent(in)  :: dxk
+      !! stepsize
+    real,    intent(in)  :: polyk(:,:)
+      !! interpolation polynomial (from previous step)
+    real,    intent(out) :: z(:)
+      !! output initial guess for z
+
+    ! local variables
+    integer :: i, j, i0, i1
+
+    ! use interpolation polynomial from previous step to extrapolate z(x) = U(x) - U_k
+    i1 = 0
+    do i = 1, 3
+      i0 = i1 + 1
+      i1 = i1 + nU
+
+      z(i0:i1) = 0
+      do j = 1, 3
+        z(i0:i1) = z(i0:i1) + polyk(:,j) * (x + C(i) * dxk - xold)**j
+      end do
+    end do
+  end subroutine
+
+  subroutine eval_f(nU, fun, x, dxk, Uk, z, P, f, dfdz, dfdP)
+    integer,                     intent(in)  :: nU
+      !! system size
+    procedure(ode_fun), pointer, intent(in)  :: fun
+      !! function to integrate
+    real,                        intent(in)  :: x
+      !! beginning of step
+    real,                        intent(in)  :: dxk
+      !! step size
+    real,                        intent(in)  :: Uk(:)
+      !! state at beginning of step
+    real,                        intent(in)  :: z(:)
+      !! delta states
+    real,                        intent(in)  :: P(:)
+      !! parameters
+    real,                        intent(out) :: f(:,:)
+      !! output function values
+    real,                        intent(out) :: dfdz(:,:,:)
+      !! output derivatives of f wrt z
+    real, optional,              intent(out) :: dfdP(:,:,:)
+      !! output derivatives of f wrt parameters
+
+    ! local variables
+    integer :: i, i0, i1
+
+    i1 = 0
+    do i = 1, 3
+      i0 = i1 + 1
+      i1 = i1 + nU
+      if (present(dfdP)) then
+        call fun(x + C(i) * dxk, Uk + z(i0:i1), P, f = f(:,i), dfdU = dfdz(:,:,i), dfdP = dfdP(:,:,i))
+      else
+        call fun(x + C(i) * dxk, Uk + z(i0:i1), P, f = f(:,i), dfdU = dfdz(:,:,i))
+      end if
+    end do
+  end subroutine
+
+  subroutine eval_h(nU, dxk, f, dfdz, z, h, dhdz)
+    integer, intent(in)  :: nU
+      !! system size
+    real,    intent(in)  :: dxk
+      !! step size
+    real,    intent(in)  :: f(:,:)
+      !! function values for all stages
+    real,    intent(in)  :: dfdz(:,:,:)
+      !! derivatives of f wrt z
+    real,    intent(in)  :: z(:)
+      !! delta states
+    real,    intent(out) :: h(:)
+      !! output residuals
+    real,    intent(out) :: dhdz(:,:)
+      !! output derivatives of h wrt z
+
+    ! local variables
+    integer :: i, j, i0, i1, j0, j1
+
+    dhdz = 0
+
+    i1 = 0
+    do i = 1, 3
+      i0 = i1 + 1
+      i1 = i1 + nU
+
+      h(i0:i1) = z(i0:i1)
+      do j = i0, i1
+        dhdz(j,j) = 1
+      end do
+
+      j1 = 0
+      do j = 1, 3
+        j0 = j1 + 1
+        j1 = j1 + nU
+
+        h(i0:i1) = h(i0:i1) - dxk * A(i,j) * f(:,j)
+        dhdz(i0:i1,j0:j1) = dhdz(i0:i1,j0:j1) - dxk * A(i,j) * dfdz(:,:,j)
+      end do
+    end do
+  end subroutine
+
+  subroutine eval_dhdQ(nU, nP, dxk, dfdz, dfdP, dUkdQ, dhdQ)
+    integer, intent(in)  :: nU
+      !! system size
+    integer, intent(in)  :: nP
+      !! number of parameters
+    real,    intent(in)  :: dxk
+      !! step size
+    real,    intent(in)  :: dfdz(:,:,:)
+      !! derivatives of f wrt z (= dfdUk)
+    real,    intent(in)  :: dfdP(:,:,:)
+      !! derivatives of f wrt parameters
+    real,    intent(in)  :: dUkdQ(:,:)
+      !! derivatives of Uk wrt Q = [U0; P]
+    real,    intent(out) :: dhdQ(3*nU,nU+nP)
+      !! output derivatives of h wrt Q = [U0; P]
+
+    ! local variables
+    integer :: i, j, i0, i1
+    real    :: dhdUk(nU,nU), dhdP(nU,nP)
+
+    i1 = 0
+    do i = 1, 3
+      i0 = i1 + 1
+      i1 = i1 + nU
+
+      dhdUk = 0
+      dhdP  = 0
+      do j = 1, 3
+        dhdUk = dhdUk - dxk * A(i,j) * dfdz(:,:,j)
+        dhdP  = dhdP -  dxk * A(i,j) * dfdP(:,:,j)
+      end do
+
+      dhdQ(i0:i1,:) = matmul(dhdUk, dUkdQ)
+      dhdQ(i0:i1,(nU+1):(nU+nP)) = dhdQ(i0:i1,(nU+1):(nU+nP)) + dhdP
+    end do
+  end subroutine
+
+  subroutine error_estimate(nU, fun, x, dxk, Uk, fk, dfkdUk, P, opt, z, Un, status, err)
+    integer,                     intent(in)  :: nU
+      !! system size
+    procedure(ode_fun), pointer, intent(in)  :: fun
+      !! function to integrate
+    real,                        intent(in)  :: x
+      !! initial x position of step
+    real,                        intent(in)  :: dxk
+      !! stepsize
+    real,                        intent(in)  :: Uk(:)
+      !! state at beginning of step
+    real,                        intent(in)  :: fk(:)
+      !! dUk/dx
+    real,                        intent(in)  :: dfkdUk(:,:)
+      !! derivatives of fk wrt Uk
+    real,                        intent(in)  :: P(:)
+      !! parameters
+    type(ode_options),           intent(in)  :: opt
+      !! options
+    real,                        intent(in)  :: z(:)
+      !! delta states
+    real,                        intent(in)  :: Un(:)
+      !! state at end of step
+    logical,                     intent(in)  :: status
+      !! old status
+    real,                        intent(out) :: err
+      !! output scalar error estimate
+
+    ! local variables
+    real    :: i, i0, i1, eU(nU,1), eU_tmp(nU), eUmat(nU,nU)
+    integer :: ipiv(nU)
+
+    ! delta U
+    eU(:,1) = G0 * dxk * fk
+    i1 = 0
+    do i = 1, 3
+      i0 = i1 + 1
+      i1 = i1 + nU
+
+      eU(:,1) = eU(:,1) + E(i) * z(i0:i1)
+    end do
+
+    ! matrix to refine error estimate
+    eUmat = - dxk * G0 * dfkdUk
+    do i = 1, nU
+      eUmat(i,i) = eUmat(i,i) + 1.0
+    end do
+
+    ! solve
+    call gesv(eUmat, eU, ipiv = ipiv)
+
+    ! if last step was rejected, refine error estimate again
+    if (.not. status) then
+      call fun(x, Uk + eU(:,1), P, f = eU_tmp)
+      eU(:,1) = G0 * dxk * eU_tmp
+      i1 = 0
+      do i = 1, 3
+        i0 = i1 + 1
+        i1 = i1 + nU
+
+        eU(:,1) = eU(:,1) + E(i) * z(i0:i1)
+      end do
+
+      ! solve again
+      call getrs(eUmat, ipiv, eU)
+    end if
+
+    ! get scalar error estimate
+    err = 0
+    do i = 1, nU
+      err = err + (eU(i,1) / (opt%atol(i) + max(abs(Uk(i)), abs(Un(i))) * opt%rtol(i)))**2
+    end do
+    err = sqrt(err / nU)
+  end subroutine
+
+  subroutine interpolate(dxk, z1, z2, z3, dz1dQ, dz2dQ, dz3dQ, polyn, dpolyndQ)
+    real, intent(in)  :: dxk
+      !! step size
+    real, intent(in)  :: z1(:)
+      !! delta state at 1st stage
+    real, intent(in)  :: z2(:)
+      !! delta state at 2nd stage
+    real, intent(in)  :: z3(:)
+      !! delta state at 3rd stage
+    real, intent(in)  :: dz1dQ(:,:)
+      !! derivatives of z1 wrt Q = [U0; P]
+    real, intent(in)  :: dz2dQ(:,:)
+      !! derivatives of z2 wrt Q = [U0; P]
+    real, intent(in)  :: dz3dQ(:,:)
+      !! derivatives of z3 wrt Q = [U0; P]
+    real, intent(out) :: polyn(:,:)
+      !! output interpolation polynomial for this step
+    real, intent(out) :: dpolyndQ(:,:,:)
+      !! output derivatives of polyn wrt Q = [U0; P]
+
+    polyn(:,1) =  (13*(z1 + z2) +    z3 +  7*sqrt(6.0)*(z1 - z2)) / (3 * dxk   )
+    polyn(:,2) = -(23*(z1 + z2) +  8*z3 + 22*sqrt(6.0)*(z1 - z2)) / (3 * dxk**2)
+    polyn(:,3) =  (10*(z1 + z2) + 10*z3 + 15*sqrt(6.0)*(z1 - z2)) / (3 * dxk**3)
+
+    dpolyndQ(:,:,1) =  (13*(dz1dQ + dz2dQ) +    dz3dQ +  7*sqrt(6.0)*(dz1dQ - dz2dQ)) / (3 * dxk   )
+    dpolyndQ(:,:,2) = -(23*(dz1dQ + dz2dQ) +  8*dz3dQ + 22*sqrt(6.0)*(dz1dQ - dz2dQ)) / (3 * dxk**2)
+    dpolyndQ(:,:,3) =  (10*(dz1dQ + dz2dQ) + 10*dz3dQ + 15*sqrt(6.0)*(dz1dQ - dz2dQ)) / (3 * dxk**3)
+  end subroutine
+
+  subroutine step_size_prediction(x, dx_old, dxk, err_old, err, opt, it, dxn)
+    real,              intent(in)  :: x
+      !! initial x position of step
+    real,              intent(in)  :: dx_old
+      !! old step size from step before
+    real,              intent(in)  :: dxk
+      !! step size
+    real,              intent(in)  :: err_old
+      !! old error estimate from step before
+    real,              intent(in)  :: err
+      !! error estimate
+    type(ode_options), intent(in)  :: opt
+      !! ode solver options
+    integer,           intent(in)  :: it
+      !! number of Newton iterations needed
+    real,              intent(out) :: dxn
+      !! output new step size
+
+    ! local variables
+    real :: fac, dxn1, dxn2
+
+    ! get safety factor
+    fac = 0.9 * (2 * opt%newton_max_it + 1.0) / (2 * opt%newton_max_it + it)
+
+    ! estimate new step size by two different methods
+    dxn1 = fac * dxk * err**(-0.25)
+    dxn2 = dxn1 * dxk / dx_old * (err_old / err)**(0.25)
+
+    ! take minimum of both results
+    dxn = min(dxn1, dxn2)
+
+    if (abs(dxn) < opt%min_rx * abs(x+dxk)) then
+      dxn = sign(opt%min_rx * abs(x+dxk), dxn)
+    end if
+  end subroutine
+
+end module
