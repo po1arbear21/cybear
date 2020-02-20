db0cfb41c8b293a5339aa937358aa1049b6ffa4d
Paul Luckner <pl@ithe.rwth-aachen.de>
2020-02-20 12:17:02 +0100

    vector: push whole array + test

--- src/test/modules/test_vector.f90
+++ src/test/modules/test_vector.f90
@@ -28,6 +28,19 @@ contains
     call tc%assert_eq(arr_exp(1), vec%first(), "first")
     call tc%assert_eq(arr_exp(5), vec%last(), "last")
 
+    ! push arrays
+    arr = [2, 4, 5, 2, 1]
+    call vec%reset
+    call vec%push(arr)
+    call vec%push(arr)
+
+    deallocate (arr_exp)
+    allocate (arr_exp(10))
+    arr_exp(1:5)  = arr
+    arr_exp(6:10) = arr
+    arr = vec%get()
+    call tc%assert_eq(arr_exp, arr, "push elems")
+
     call tc%finish
   end subroutine
 
