dc753eebf36b0748e356ef5341333a97bc5210d6
Tobias Linn <tl@ithe.rwth-aachen.de>
2020-03-03 10:38:47 +0100

    input test: always get section ids before using them

--- src/test/modules/test_input.f90
+++ src/test/modules/test_input.f90
@@ -7,10 +7,8 @@ module test_input_m
 contains
 
   subroutine test_input
-    type(test_case) :: tc
-
-    type(input_file)  :: f
-    integer, allocatable :: sid(:)
+    type(test_case)  :: tc
+    type(input_file) :: f
 
     print "(1A)", "test_input"
     call tc%init("input")
@@ -20,9 +18,10 @@ contains
 
     ! reals (+ check normalization implicitly)
     block
-      integer           :: i
-      real              :: r
-      real, allocatable :: r_arr(:)
+      integer              :: i
+      real                 :: r
+      real,    allocatable :: r_arr(:)
+      integer, allocatable :: sid(:)
 
       ! check get_name_real + init normalization
       call f%get("", "temperature", r, normalize=.false.)
@@ -50,6 +49,7 @@ contains
     block
       integer              :: i
       integer, allocatable :: i_arr(:)
+      integer, allocatable :: sid(:)
 
       ! check get_name_int
       call f%get("grid", "Nx", i)
@@ -60,10 +60,12 @@ contains
       call tc%assert_eq([2, 4, 3, 2, 2, -1], i_arr, "get_name_int_arr")
 
       ! check get_int
+      call f%get_sections("grid", sid)
       call f%get(sid(1), "Nx", i)
       call tc%assert_eq(100, i, "get_int")
 
       ! check get_int_arr
+      call f%get_sections("grid", sid)
       call f%get(sid(1), "idx", i_arr)
       call tc%assert_eq([2, 4, 3, 2, 2, -1], i_arr, "get_int_arr")
     end block
@@ -72,19 +74,20 @@ contains
     block
       character(:), allocatable :: c_arr
       type(string), allocatable :: s_arr(:)
+      integer,      allocatable :: sid(:)
 
       ! get_name_string
       call f%get("contact", "name", c_arr)
       call tc%assert_eq(string("GAT"), string(c_arr), "get_name_string")
 
+      ! get_name_string_arr
+      call f%get("contact", "names", s_arr)
+      call tc%assert_eq([string("123"), string("asd asd"), string("asd 123 x")], s_arr, "get_name_string_arr")
+
       ! get_string
       call f%get_sections("contact", sid)
       call f%get(sid(1), "name", c_arr)
       call tc%assert_eq(string("GAT"), string(c_arr), "get_string")
-
-      ! get_name_string_arr
-      call f%get("contact", "names", s_arr)
-      call tc%assert_eq([string("123"), string("asd asd"), string("asd 123 x")], s_arr, "get_name_string_arr")
 
       ! get_string_arr
       call f%get_sections("contact", sid)
@@ -96,6 +99,7 @@ contains
     block
       logical              :: tf
       logical, allocatable :: tf_arr(:)
+      integer, allocatable :: sid(:)
 
       ! check get_name_logical
       call f%get("contact", "gate", tf)
@@ -111,6 +115,7 @@ contains
       call tc%assert_eq(count([.true.]), count([tf]), "get_logical")
 
       ! check get_logical_arr
+      call f%get_sections("contact", sid)
       call f%get(sid(1), "tf", tf_arr)
       call tc%assert_eq(count(tf_arr), count([.true., .true., .false., .true.]), "get_logical_arr")
     end block
