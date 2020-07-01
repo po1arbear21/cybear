513b1441cf5bb022ee1c1e399d0aed91927070d2
Paul Luckner <pl@ithe.rwth-aachen.de>
2020-07-01 15:05:49 +0200

    ilupack: complex test

--- src/test/modules/test_ilupack.f90
+++ src/test/modules/test_ilupack.f90
@@ -68,55 +68,6 @@ contains
       call ilu%delete
     end block
 
-    ! test ilupack_cmplx: identity matrix
-    block
-      type(ilupack_cmplx)  :: ilu
-      integer              :: n, n_rhs, i_rhs
-      integer, allocatable :: ia(:), ja(:)
-      complex, allocatable ::  a(:), b(:,:), x_exp(:,:), x(:)
-
-      !
-      ! build matrix
-      !
-      ! M = I_3
-      !
-      n  = 3
-      a  = [ (1.0,0.0), (1.0,0.0), (1.0,0.0) ]
-      ja = [   1,   2,   3      ]
-      ia = [   1,   2,   3,   4 ]
-
-
-      !
-      ! rhs
-      !
-      n_rhs = 3
-      allocate (b(n,n_rhs))
-      b(:,1) = 0
-      b(:,2) = [ (1.0,1.0), (2.0,1.0), (0.0,1.0) ]
-      b(:,3) = [ (1.0,0.0), (2.0,1.0), (3.0,1.0) ]
-
-
-      !
-      ! sols
-      !
-      allocate (x_exp(n,n_rhs), x(n))
-      x_exp = b
-
-      !
-      ! ilu: init -> factor -> solve -> delete
-      !
-      call ilu%init(ia, ja, a)
-      call ilu%factor
-
-      do i_rhs = 1, n_rhs
-        x = x_exp(1,i_rhs)
-        call ilu%solve(b(:,i_rhs), x)
-        call tc%assert_eq(x_exp(:,i_rhs), x, 1e-13, "complex solving")
-      end do
-
-      call ilu%delete
-    end block
-
     ! test ilupack_cmplx
     block
       type(ilupack_cmplx)  :: ilu
@@ -136,7 +87,6 @@ contains
       ja = [   1,   4,   1,   2,   3,   2,   4 ]
       ia = [   1,   3,   5,   6,   8 ]
 
-
       !
       ! rhs
       !
@@ -146,14 +96,13 @@ contains
       b(:,2) = [ (1.0,1.0), (2.0,1.0), (0.0,1.0), (1.0,-2.0) ]
       b(:,3) = [ (1.0,0.0), (2.0,1.0), (3.0,1.0), (4.0, 0.0) ]
 
-
       !
       ! sols
       !
       allocate (x_exp(n,n_rhs), x(n))
       x_exp(:,1) = 0
-      x_exp(:,2) = [ (-0.240000000000000,-0.680000000000000), (0.400000000000000,0.300000000000000), (-0.333333333333333, 0.000000000000000), (-0.440000000000000,-0.080000000000000) ]
-      x_exp(:,3) = [ (-0.100000000000000,-0.300000000000000), (0.312500000000000,0.062500000000000), (-0.333333333333333,-1.000000000000000), ( 0.300000000000000,-0.100000000000000) ]
+      x_exp(:,2) = [ ( 0.600000000000000, 0.000000000000000), (-0.125000000000000,0.125000000000000), (0.333333333333333, 0.000000000000000), (0.400000000000000,-0.600000000000000) ]
+      x_exp(:,3) = [ (-0.020000000000000,-0.140000000000000), ( 0.262500000000000,0.212500000000000), (0.333333333333333,-1.000000000000000), (0.380000000000000,-0.340000000000000) ]
 
       !
       ! ilu: init -> factor -> solve -> delete
@@ -162,15 +111,8 @@ contains
       call ilu%factor
 
       do i_rhs = 1, n_rhs
-        x = x_exp(1,i_rhs)
+        x = 0
         call ilu%solve(b(:,i_rhs), x)
-        print *, 'rhs'
-        print *, b(:,i_rhs)
-        print *, 'expected'
-        print *, x_exp(:,i_rhs)
-        print *, 'solution'
-        print *, x
-        print *
         call tc%assert_eq(x_exp(:,i_rhs), x, 1e-13, "complex solving")
       end do
 
