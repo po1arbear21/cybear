f58a05154de8aa626916c82de4f9982ffb2995cc
Paul Luckner <pl@ithe.rwth-aachen.de>
2020-07-01 14:17:17 +0200

    ilupack: restructured + complex

--- src/lib/ilupack.f90
+++ src/lib/ilupack.f90
@@ -3,110 +3,24 @@
 module ilupack_m
   !! wrapper around ILUPACK. uses interfaces for ILUPACK defined in ilupack_interf_m.
 
-  use ilupack_interfaces_m
   use error_m
   use util_m
 
   implicit none
 
   private
-  public :: ilupack
+  public :: ilupack_real
+  public :: ilupack_cmplx
 
-  type ilupack
-    !! ilupack wrapper.
-    !! usage:
-    !!    1. call init: sets default options
-    !!    [1b. overwrite option values manually]
-    !!    2. call factor
-    !!    [2a. call info: writes info about multilevel structure]
-    !!    [2b. get fill-in-factor]
-    !!    3. call solve
-    !!    [3b. call solver for multiple rhs]
-    !!    4. call delete
+#define T0 D
+#define T  real
+#define TT real
+#include "ilupack_def.f90.inc"
 
-    !
-    ! ilupack options (can be set manually)
-    !
-    character(20) :: ordering
-      !! string indicating which reordering to apply
-      !! multilevel orderings
-      !! 'amd'           Approximate Minimum Degree
-      !! 'mmd'           Minimum Degree
-      !! 'rcm'           Reverse Cuthill-McKee
-      !! 'metisn'        Metis multilevel nested dissection by nodes
-      !! 'metise'        Metis multilevel nested dissection by edges
-      !! 'pq'            ddPQ strategy by Saad
-      !! default: 'amd
-    real          :: elbow
-      !! elbow space factor for the fill computed during the ILU
-      !! default: 10
-    integer       :: lfil
-      !! number of fill elements per column in L resp. rows in U
-      !! default: n+1
-    integer       :: lfilS
-      !! maximum number of nonzeros per row in the approximate Schur complement
-      !! default: n+1
-    integer       :: matching
-      !! maximum weight matching
-      !! default: 1 == is different from zero == matching turned on
-    integer       :: maxit
-      !! maximum number of iterative steps
-      !! default: (500??)
-    integer       :: nrestart
-      !! restart length for GMRES
-      !! default: 30
-    real          :: droptol
-      !! threshold for ILU
-      !! default: 1e-2
-    real          :: droptolS
-      !! threshold for the approximate Schur complements
-      !! info: should be a smaller than droptol (~10%?)
-      !! default: 1e-3
-    real          :: condest
-      !! norm bound for the inverse factors L^{-1}, U^{-1}
-      !! default: 1e2
-    real          :: restol
-      !! relative error for the backward error (SPD case: relative energy norm) used during the iterative solver
-      !! default: sqrt(eps)
-    integer       :: mixedprecision
-      !! use single precision preconditioner if == 1
-      !! default: 0
-
-    !
-    ! parameters (should not be set manually)
-    !
-    integer :: n
-      !! size of the system. matrix is of (nxn) size.
-    integer :: it
-      !! number of iterations at solver step
-
-
-    !
-    ! ilupack workspace variables
-    !
-    integer, allocatable :: ind(:)
-      !! array of size n
-      !! default: 0
-    integer              :: param
-      !! parameter C-pointer casted to integer
-    integer              :: prec
-      !! preconditioner C-pointer casted to integer
-
-    !
-    ! copy of ia,ja,a. factor rescales them...
-    !
-    integer, allocatable :: ia(:), ja(:)
-    real,    allocatable :: a(:)
-
-  contains
-    procedure :: init       => ilupack_init
-    procedure :: factor     => ilupack_factor
-    procedure :: info       => ilupack_info
-    procedure :: nnz        => ilupack_nnz
-    procedure :: solve      => ilupack_solve
-    procedure :: delete     => ilupack_delete
-    procedure :: print_opts => ilupack_print_opts
-  end type
+#define T0 Z
+#define T  cmplx
+#define TT complex
+#include "ilupack_def.f90.inc"
 
   character(*), parameter :: ILUPACK_FACTOR_ERROR(-7:-1) = [  &
     & "buffers are too small                               ", &
@@ -122,112 +36,17 @@ module ilupack_m
     & "not enough work space                               ", &
     & "too many iterations                                 "  ]
 
+
 contains
 
-  subroutine ilupack_init(this, ia, ja, a)
-    !! init ilupack options.
+#define T0 D
+#define T  real
+#define TT real
+#include "ilupack_imp.f90.inc"
 
-    class(ilupack), intent(out) :: this
-    integer,        intent(in)  :: ia(:)
-    integer,        intent(in)  :: ja(:)
-    real,           intent(in)  ::  a(:)
-
-    ASSERT(size(a) == size(ja))
-
-    this%n = size(ia)-1
-    allocate (this%ind(this%n), source=0)
-
-    this%ia = ia
-    this%ja = ja
-    this%a  = a
-
-    call dgnlamginit(this%n,              this%ia,      this%ja,       this%a,       this%matching, &
-      &              this%ordering,       this%droptol, this%droptolS, this%condest, this%restol,   &
-      &              this%maxit,          this%elbow,   this%lfil,     this%lfilS,   this%nrestart, &
-      &              this%mixedprecision, this%ind                                                  )
-
-  end subroutine
-
-  subroutine ilupack_print_opts(this)
-    !! print ilupack options.
-
-    class(ilupack), intent(in) :: this
-
-    print '(A)',        'ordering:      "' // this%ordering // '"'
-    print '(A,E24.16)', 'elbow:          ',   this%elbow
-    print '(A,I24)',    'lfil:           ',   this%lfil
-    print '(A,I24)',    'lfilS:          ',   this%lfilS
-    print '(A,I24)',    'matching:       ',   this%matching
-    print '(A,I24)',    'maxit:          ',   this%maxit
-    print '(A,I24)',    'nrestart:       ',   this%nrestart
-    print '(A,E24.16)', 'droptol:        ',   this%droptol
-    print '(A,E24.16)', 'droptolS:       ',   this%droptolS
-    print '(A,E24.16)', 'condest:        ',   this%condest
-    print '(A,E24.16)', 'restol:         ',   this%restol
-    print '(A,I24)',    'mixedprecision: ',   this%mixedprecision
-  end subroutine
-
-  subroutine ilupack_factor(this)
-    class(ilupack), intent(inout) :: this
-
-    integer :: ierr
-
-    ierr = dgnlamgfactor(this%param,          this%prec,                                                &
-      &                  this%n,              this%ia,      this%ja,       this%a,       this%matching, &
-      &                  this%ordering,       this%droptol, this%droptolS, this%condest, this%restol,   &
-      &                  this%maxit,          this%elbow,   this%lfil,     this%lfilS,   this%nrestart, &
-      &                  this%mixedprecision, this%ind                                                  )
-
-    if      ((ierr > -8) .and. (ierr < 0)) then
-      call program_error("Error in ilupack: " // trim(ILUPACK_FACTOR_ERROR(ierr)))
-    else if ( ierr /= 0                  ) then
-      call program_error("Error in ilupack: zero pivot encountered at step number " // int2str(ierr))
-    end if
-  end subroutine
-
-  subroutine ilupack_info(this)
-    class(ilupack), intent(in) :: this
-
-    call dgnlamginfo(this%param, this%prec, this%n, this%ia, this%ja, this%a)
-  end subroutine
-
-  integer function ilupack_nnz(this)
-    class(ilupack), intent(in) :: this
-
-    ilupack_nnz = dgnlamgnnz(this%param, this%prec)
-  end function
-
-  subroutine ilupack_solve(this, rhs, sol)
-    class(ilupack), intent(inout) :: this
-    real,           intent(in)    :: rhs(:)
-      !! right hand side  of length n
-    real,           intent(inout) :: sol(:)
-      !! solution vector of length n, must be initialized on input with an initial guess
-
-    integer :: ierr
-
-    ASSERT(size(rhs) == this%n)
-    ASSERT(size(sol) == this%n)
-
-    this%it = this%maxit
-
-    ierr = dgnlamgsolver(this%param,          this%prec,    rhs,           sol,                         &
-      &                  this%n,              this%ia,      this%ja,       this%a,       this%matching, &
-      &                  this%ordering,       this%droptol, this%droptolS, this%condest, this%restol,   &
-      &                  this%it,             this%elbow,   this%lfil,     this%lfilS,   this%nrestart, &
-      &                  this%mixedprecision, this%ind                                                  )
-
-    if      ((ierr >= -3) .and. (ierr < 0)) then
-      call program_error("Error in ilupack: " // trim(ILUPACK_SOLVER_ERROR(ierr)))
-    else if ( ierr /=  0                  ) then
-      call program_error("Error in ilupack: solver exited with error code " // int2str(ierr))
-    end if
-  end subroutine
-
-  subroutine ilupack_delete(this)
-    class(ilupack), intent(inout) :: this
-
-    call dgnlamgdelete(this%param, this%prec)
-  end subroutine
+#define T0 Z
+#define T  cmplx
+#define TT complex
+#include "ilupack_imp.f90.inc"
 
 end module
--- src/test/modules/test_ilupack.f90
+++ src/test/modules/test_ilupack.f90
@@ -8,61 +8,174 @@ module test_ilupack_m
 contains
 
   subroutine test_ilupack
-    type(test_case)      :: tc
-    type(ilupack)        :: ilu
-    integer              :: n, n_rhs, i_rhs
-    integer, allocatable :: ia(:), ja(:)
-    real,    allocatable :: a(:), b(:,:), x_exp(:,:), x(:)
+    type(test_case) :: tc
 
     print "(1A)", "test_ilupack"
     call tc%init("ilupack")
 
-    !
-    ! build matrix
-    !     / 2 0 0 1 \
-    ! M = | 5 8 0 0 |
-    !     | 0 0 3 0 |
-    !     \ 0 8 0 5 /
-    !
-    n  = 4
-    a  = [ 2.0, 1.0, 5.0, 8.0, 3.0, 8.0, 5.0 ]
-    ja = [   1,   4,   1,   2,   3,   2,   4 ]
-    ia = [   1,   3,   5,   6,   8 ]
+    ! test ilupack_real
+    block
+      type(ilupack_real)   :: ilu
+      integer              :: n, n_rhs, i_rhs
+      integer, allocatable :: ia(:), ja(:)
+      real,    allocatable :: a(:), b(:,:), x_exp(:,:), x(:)
 
-    !
-    ! rhs
-    !
-    n_rhs = 5
-    allocate (b(n,n_rhs))
-    b(:,1) = [ 1.0, 2.0, 3.0, 4.0 ]
-    b(:,2) = [ 1.0, 3.0, 3.0, 4.0 ]
-    b(:,3) = [ 9.0, 2.0, 3.0, 4.0 ]
-    b(:,4) = [ 3.0, 1.0, 5.0, 6.0 ]
-    b(:,5) = [ 1.0, 3.0, 7.0, 2.0 ]
+      !
+      ! build matrix
+      !     / 2 0 0 1 \
+      ! M = | 5 8 0 0 |
+      !     | 0 0 3 0 |
+      !     \ 0 8 0 5 /
+      !
+      n  = 4
+      a  = [ 2.0, 1.0, 5.0, 8.0, 3.0, 8.0, 5.0 ]
+      ja = [   1,   4,   1,   2,   3,   2,   4 ]
+      ia = [   1,   3,   5,   6,   8 ]
 
-    !
-    ! sols
-    !
-    allocate (x_exp(n,n_rhs), x(n))
-    x_exp(:,1) = [ 0.200000000000000,  0.125000000000000, 1.000000000000000, 0.600000000000000 ]
-    x_exp(:,2) = [ 0.266666666666667,  0.208333333333333, 1.000000000000000, 0.466666666666667 ]
-    x_exp(:,3) = [ 2.866666666666666, -1.541666666666667, 1.000000000000000, 3.266666666666667 ]
-    x_exp(:,4) = [ 0.666666666666667, -0.291666666666667, 1.666666666666667, 1.666666666666667 ]
-    x_exp(:,5) = [ 0.400000000000000,  0.125000000000000, 2.333333333333333, 0.200000000000000 ]
+      !
+      ! rhs
+      !
+      n_rhs = 5
+      allocate (b(n,n_rhs))
+      b(:,1) = [ 1.0, 2.0, 3.0, 4.0 ]
+      b(:,2) = [ 1.0, 3.0, 3.0, 4.0 ]
+      b(:,3) = [ 9.0, 2.0, 3.0, 4.0 ]
+      b(:,4) = [ 3.0, 1.0, 5.0, 6.0 ]
+      b(:,5) = [ 1.0, 3.0, 7.0, 2.0 ]
 
-    !
-    ! ilu: init -> factor -> solve -> delete
-    !
-    call ilu%init(ia, ja, a)
-    call ilu%factor
+      !
+      ! sols
+      !
+      allocate (x_exp(n,n_rhs), x(n))
+      x_exp(:,1) = [ 0.200000000000000,  0.125000000000000, 1.000000000000000, 0.600000000000000 ]
+      x_exp(:,2) = [ 0.266666666666667,  0.208333333333333, 1.000000000000000, 0.466666666666667 ]
+      x_exp(:,3) = [ 2.866666666666666, -1.541666666666667, 1.000000000000000, 3.266666666666667 ]
+      x_exp(:,4) = [ 0.666666666666667, -0.291666666666667, 1.666666666666667, 1.666666666666667 ]
+      x_exp(:,5) = [ 0.400000000000000,  0.125000000000000, 2.333333333333333, 0.200000000000000 ]
 
-    do i_rhs = 1, n_rhs
-      x = x_exp(1,i_rhs)
-      call ilu%solve(b(:,i_rhs), x)
-      call tc%assert_eq(x_exp(:,i_rhs), x, 1e-13, "solving")
-    end do
+      !
+      ! ilu: init -> factor -> solve -> delete
+      !
+      call ilu%init(ia, ja, a)
+      call ilu%factor
 
-    call ilu%delete
+      do i_rhs = 1, n_rhs
+        x = x_exp(1,i_rhs)
+        call ilu%solve(b(:,i_rhs), x)
+        call tc%assert_eq(x_exp(:,i_rhs), x, 1e-13, "real solving")
+      end do
+
+      call ilu%delete
+    end block
+
+    ! test ilupack_cmplx: identity matrix
+    block
+      type(ilupack_cmplx)  :: ilu
+      integer              :: n, n_rhs, i_rhs
+      integer, allocatable :: ia(:), ja(:)
+      complex, allocatable ::  a(:), b(:,:), x_exp(:,:), x(:)
+
+      !
+      ! build matrix
+      !
+      ! M = I_3
+      !
+      n  = 3
+      a  = [ (1.0,0.0), (1.0,0.0), (1.0,0.0) ]
+      ja = [   1,   2,   3,   4 ]
+      ia = [   1,   2,   3,   4,   5 ]
+
+
+      !
+      ! rhs
+      !
+      n_rhs = 3
+      allocate (b(n,n_rhs))
+      b(:,1) = 0
+      b(:,2) = [ (1.0,1.0), (2.0,1.0), (0.0,1.0), (1.0,-2.0) ]
+      b(:,3) = [ (1.0,0.0), (2.0,1.0), (3.0,1.0), (4.0, 0.0) ]
+
+
+      !
+      ! sols
+      !
+      allocate (x_exp(n,n_rhs), x(n))
+      x_exp = b
+
+      !
+      ! ilu: init -> factor -> solve -> delete
+      !
+      call ilu%init(ia, ja, a)
+      call ilu%factor
+
+      do i_rhs = 1, n_rhs
+        x = x_exp(1,i_rhs)
+        call ilu%solve(b(:,i_rhs), x)
+        call tc%assert_eq(x_exp(:,i_rhs), x, 1e-13, "complex solving")
+      end do
+
+      call ilu%delete
+    end block
+
+    ! test ilupack_cmplx
+    block
+      type(ilupack_cmplx)  :: ilu
+      integer              :: n, n_rhs, i_rhs
+      integer, allocatable :: ia(:), ja(:)
+      complex, allocatable :: a(:), b(:,:), x_exp(:,:), x(:)
+
+      !
+      ! build matrix
+      !     / 2i 0 0  1+i \
+      ! M = | 5  8 0  0   |
+      !     | 0  0 3i 0   |
+      !     \ 0  8 0  5   /
+      !
+      n  = 4
+      a  = [ (0.0,2.0), (1.0,1.0), (5.0,0.0), (8.0,0.0), (0.0,3.0), (8.0,0.0), (5.0,0.0) ]
+      ja = [   1,   4,   1,   2,   3,   2,   4 ]
+      ia = [   1,   3,   5,   6,   8 ]
+
+
+      !
+      ! rhs
+      !
+      n_rhs = 3
+      allocate (b(n,n_rhs))
+      b(:,1) = 0
+      b(:,2) = [ (1.0,1.0), (2.0,1.0), (0.0,1.0), (1.0,-2.0) ]
+      b(:,3) = [ (1.0,0.0), (2.0,1.0), (3.0,1.0), (4.0, 0.0) ]
+
+
+      !
+      ! sols
+      !
+      allocate (x_exp(n,n_rhs), x(n))
+      x_exp(:,1) = 0
+      x_exp(:,2) = [ (-0.240000000000000,-0.680000000000000), (0.400000000000000,0.300000000000000), (-0.333333333333333, 0.000000000000000), (-0.440000000000000,-0.080000000000000) ]
+      x_exp(:,3) = [ (-0.100000000000000,-0.300000000000000), (0.312500000000000,0.062500000000000), (-0.333333333333333,-1.000000000000000), ( 0.300000000000000,-0.100000000000000) ]
+
+      !
+      ! ilu: init -> factor -> solve -> delete
+      !
+      call ilu%init(ia, ja, a)
+      call ilu%factor
+
+      do i_rhs = 1, n_rhs
+        x = x_exp(1,i_rhs)
+        call ilu%solve(b(:,i_rhs), x)
+        print *, 'rhs'
+        print *, b(:,i_rhs)
+        print *, 'expected'
+        print *, x_exp(:,i_rhs)
+        print *, 'solution'
+        print *, x
+        print *
+        call tc%assert_eq(x_exp(:,i_rhs), x, 1e-13, "complex solving")
+      end do
+
+      call ilu%delete
+    end block
 
     call tc%finish
   end subroutine
