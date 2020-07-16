cf96006cdf5fd2bf2d2e7bb77ce488daf6639daf
Paul Luckner <pl@ithe.rwth-aachen.de>
2020-07-16 10:28:39 +0200

    minor: typesetting

--- src/util/linalg/arnoldi.f90
+++ src/util/linalg/arnoldi.f90
@@ -11,24 +11,25 @@ module arnoldi_m
   public :: arnoldi
 
 contains
+
   subroutine arnoldi(A, b, H, Q, brkd, tol, m0)
     !! Computes a basis of the (n + 1)-Krylov subspace of A: the space spanned by {b, Ab, ..., A^n b}.
 
-    class(matop_real) , intent(in)            :: A
+    class(matop_real) , intent(in)    :: A
       !! A: m  m array
-    real              , intent(in)            :: b(:)
+    real              , intent(in)    :: b(:)
       !! size: m
-    type(dense_real)  , intent(inout)         :: Q
+    type(dense_real)  , intent(inout) :: Q
       !! Q: m x (n + 1) array, the columns are an orthonormal basis of the Krylov subspace.
-    type(dense_real)  , intent(inout)         :: H      ! fixme change type: dense to hessenberg
+    type(dense_real)  , intent(inout) :: H      ! fixme change type: dense to hessenberg
       !! h: (n + 1) x n array, A on basis Q. It is upper Hessenberg.
-    integer           , intent(out)           :: brkd
+    integer           , intent(out)   :: brkd
       !! breakdown happend at k=brkd. brkd=0 means no breakdown happend.
-    real              , intent(in), optional  :: tol
+    real,    optional , intent(in)    :: tol
       !! tolerance for breakdown check
       !!      H%d(k+1,k) < k*tol*nrm2(H%d(1:k+1,k))
       !! default: 1e-12
-    integer           , intent(in), optional  :: m0
+    integer, optional , intent(in)    :: m0
       !! extend arnoldi decomposition of dimension m0 to m.
       !! Q(:,j) for j=1..m0 is set.
       !! default: 0 (<=> no column of Q is set)
@@ -57,7 +58,7 @@ contains
     q_tmp = b / nrm2(b)
     Q%d(1,:) = q_tmp
 
-    allocate(v(m))
+    allocate (v(m))
     do k = m0_+1, n
       Q%d(k,:) = q_tmp
       call A%exec(q_tmp, v)                         ! v <- A * qtmp
@@ -79,4 +80,5 @@ contains
     end do
     Q%d = transpose(Q%d)
   end subroutine
+
 end module
