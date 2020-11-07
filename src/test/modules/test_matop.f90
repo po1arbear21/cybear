6f9877f075bf612baf2cd2f7ad950a6f5dd4ddd5
Paul Luckner <pl@ithe.rwth-aachen.de>
2020-11-07 23:33:19 +0100

    matop: matop_c2r

--- src/test/modules/test_matop.f90
+++ src/test/modules/test_matop.f90
@@ -6,10 +6,12 @@ module test_matop_m
 
   implicit none
 
+  private
+  public test_matop
 
 contains
 
-  subroutine test_matop
+  subroutine test_matop()
     type(test_case) :: tc
 
     print "(A)", "test_matop"
@@ -143,7 +145,60 @@ contains
       call tc%assert_eq(y2_exp, y2, 1e-13, "single_matop_cmplx: exec2 == solve_mat")
     end block
 
-    call tc%finish
+    call test_matop_c2r(tc)
+
+    call tc%finish()
+  end subroutine
+
+  subroutine test_matop_c2r(tc)
+    !! tests type matop_c2r
+
+    type(test_case), intent(inout) :: tc
+
+    type(dense_cmplx)        :: d
+    type(single_matop_cmplx) :: sop
+    type(matop_c2r)          :: c2r
+
+    ! dense matrix:
+    ! d = [    i  0  ]
+    !     [0.5+i   -i]
+    call d%init(reshape([(0,1), (0.5,1), (0,0), (0,-1)], [2, 2]))
+    call sop%init(d)
+    call c2r%init(sop)
+
+    ! test exec1
+    block
+      ! y=d*x for x=[1+2i] => x2r = [1, 2, 3, 4]^T
+      !             [3+4i]
+      !  => y = [-2  +i] => y2r = [-2, 1, 2.5, -1]^T
+      !         [ 2.5-i]
+
+      real :: x2r(4), y2r(4), y2r_exp(4)
+
+      x2r = [1, 2, 3, 4]
+      call c2r%exec(x2r, y2r)
+      y2r_exp = [-2.0, 1.0, 2.5, -1.0]
+
+      call tc%assert_eq(y2r_exp, y2r, 1e-13, "matop_c2r: exec1")
+    end block
+
+    ! test exec2
+    block
+      ! y=d*x for x=[1+2i, 5+6i] => x2r = [1, 2, 3, 4]^T
+      !             [3+4i, 7+8i]          [5, 6, 7, 8]
+      !  => y = [-2  +i, -6  +5i] => y2r = [-2, 1, 2.5, -1]^T
+      !         [ 2.5-i,  4.5+ i]          [-6, 5, 4.5,  1]
+
+      real :: x2r(4,2), y2r(4,2), y2r_exp(4,2)
+
+      x2r(:,1) = [1, 2, 3, 4]
+      x2r(:,2) = [5, 6, 7, 8]
+      call c2r%exec(x2r, y2r)
+      y2r_exp(:,1) = [-2.0, 1.0, 2.5, -1.0]
+      y2r_exp(:,2) = [-6.0, 5.0, 4.5,  1.0]
+
+      call tc%assert_eq(y2r_exp, y2r, 1e-13, "matop_c2r: exec2")
+    end block
   end subroutine
 
 end module
--- src/util/linalg/matop.f90
+++ src/util/linalg/matop.f90
@@ -1,7 +1,10 @@
 #include "../macro.f90.inc"
 
 module matop_m
+  use error_m
+  use iso_c_binding,  only: c_loc, c_f_pointer
   use matrix_m
+
   implicit none
 
   private
@@ -11,6 +14,7 @@ module matop_m
   public single_matop_cmplx
   public chain_matop_real
   public chain_matop_cmplx
+  public matop_c2r
 
 #define T real
 #define TT real
@@ -19,6 +23,21 @@ module matop_m
 #define T cmplx
 #define TT complex
 #include "matop_def.f90.inc"
+
+  type, extends(matop_real) :: matop_c2r
+    !! a complex matop which gets its input arguments in the exec calls as real arrays of double length.
+    !! real and imaginary parts are included in the real array separately.
+    !!
+    !! use case: implementaion of zfgmres.
+
+    class(matop_cmplx), pointer :: mop_c => null()
+      !! complex matop
+  contains
+    procedure :: matop_c2r_init
+    generic   :: init  => matop_c2r_init
+    procedure :: exec1 => matop_c2r_exec1
+    procedure :: exec2 => matop_c2r_exec2
+  end type
 
 contains
 
@@ -29,5 +48,52 @@ contains
 #define T cmplx
 #define TT complex
 #include "matop_imp.f90.inc"
+
+  subroutine matop_c2r_init(this, mop_c)
+    ! initialzes by setting pointer to complex matop
+
+    class(matop_c2r),   intent(out)        :: this
+    class(matop_cmplx), intent(in), target :: mop_c
+      !! underlying complex matop
+
+    ! init base
+    call this%init("", mop_c%nrows, mop_c%ncols)
+
+    this%mop_c => mop_c
+  end subroutine
+
+  subroutine matop_c2r_exec1(this, x, y)
+    !! compute mul_vec of input and saved mat_op.
+
+    class(matop_c2r), intent(in)  :: this
+    real, target,     intent(in)  :: x(:)
+      !! input vector: real and imaginary parts.
+    real, target,     intent(out) :: y(:)
+      !! output vector: real and imaginary parts.
+
+    complex, pointer :: x_ptr(:), y_ptr(:)
+
+    ! create complex pointer to x, y
+    call c_f_pointer(c_loc(x), x_ptr, shape=shape(x)/2)
+    call c_f_pointer(c_loc(y), y_ptr, shape=shape(y)/2)
+
+    call this%mop_c%exec(x_ptr, y_ptr)
+  end subroutine
+
+  subroutine matop_c2r_exec2(this, x, y)
+    !! compute mul_mat of input and saved mat_op.
+
+    class(matop_c2r), intent(in)  :: this
+    real, target,     intent(in)  :: x(:,:)
+    real, target,     intent(out) :: y(:,:)
+
+    complex, pointer :: x_ptr(:,:), y_ptr(:,:)
+
+    ! create complex pointer to x, y
+    call c_f_pointer(c_loc(x), x_ptr, shape=[size(x, dim=1)/2, size(x, dim=2)])
+    call c_f_pointer(c_loc(y), y_ptr, shape=[size(y, dim=1)/2, size(y, dim=2)])
+
+    call this%mop_c%exec(x_ptr, y_ptr)
+  end subroutine
 
 end module
