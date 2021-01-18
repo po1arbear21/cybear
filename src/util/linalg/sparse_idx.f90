4fe1c48c5451c9478e5752d0e425df91726a089d
Paul Luckner <paul.luckner@rwth-aachen.de>
2021-01-18 15:29:35 +0100

    sparse: idx by iso_fortran_env; kind bug

--- src/util/linalg/sparse_idx.f90
+++ src/util/linalg/sparse_idx.f90
@@ -1,15 +1,15 @@
 module sparse_idx_m
-!  use iso_fortran_env ! gives warnings when real size is 8
+  use iso_fortran_env, only: int32, int64
   implicit none
 
   private
-  public :: SPARSE_IDX
+  public SPARSE_IDX
 
   ! 32 or 64 bit integers for sparse matrix indices
 #ifdef IDXSIZE32
-  integer, parameter :: SPARSE_IDX = 4 ! int32
+  integer, parameter :: SPARSE_IDX = int32
 #endif
 #ifdef IDXSIZE64
-  integer, parameter :: SPARSE_IDX = 8 ! int64
+  integer, parameter :: SPARSE_IDX = int64
 #endif
 end module
