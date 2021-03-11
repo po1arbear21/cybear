f9db9cdc7eb0252a37fb81cb7e85b162170130cf
Paul Luckner <pl@ithe.rwth-aachen.de>
2021-03-11 12:00:13 +0100

    test: formatting

--- src/test/test.f90
+++ src/test/test.f90
@@ -1,62 +1,50 @@
 program test
 
-  use test_gmres_m
-  use test_matop_m
-#ifdef USE_MUMPS
-  use test_mumps_m
-#endif
-#ifdef USE_ILUPACK
-  use test_ilupack_m
-#endif
-  use test_poly_m
-  use test_random_m
-  use test_high_precision_m
-  use test_math_m
-  use test_deque_m
-  use test_input_m
-  use test_expm_m
-  use test_normalization_m
-  use test_dual_m
-  use test_radau5_m
-  use test_newton_m
-  use test_matrix_m
-  use test_qsort_m
-  use test_vector_m
-  use test_sqrtm_m
   use test_arnoldi_m
-  use test_schur_m
+  use test_deque_m
+  use test_dual_m
+  use test_expm_m
+  use test_gmres_m
+  use test_high_precision_m
+  use test_input_m
+  use test_matop_m
   use test_plotmtv_m
+  use test_poly_m
+  use test_qsort_m
+  use test_radau5_m
+  use test_random_m
+  use test_math_m
+  use test_matrix_m
+  use test_newton_m
+  use test_normalization_m
+  use test_schur_m
+  use test_sqrtm_m
   use test_util_m
+  use test_vector_m
 
   implicit none
 
-  call test_gmres()
-  call test_matop()
-#ifdef USE_MUMPS
-  call test_mumps()
-#endif
-#ifdef USE_ILUPACK
-  call test_ilupack()
-#endif
-  call test_poly()
-  call test_random()
-  call test_high_precision()
-  call test_math()
-  call test_deque()
-  call test_input()
-  call test_expm()
-  call test_normalization()
-  call test_dual()
-  call test_radau5()
-  call test_newton()
-  call test_matrix()
-  call test_qsort()
-  call test_vector()
-  call test_sqrtm()
   call test_arnoldi()
-  call test_schur()
+  call test_deque()
+  call test_dual()
+  call test_expm()
+  call test_gmres()
+  call test_high_precision()
+  call test_input()
+  call test_matop()
   call test_plotmtv()
+  call test_poly()
+  call test_qsort()
+  call test_radau5()
+  call test_random()
+  call test_math()
+  call test_matrix()
+  call test_newton()
+  call test_normalization()
+  call test_schur()
+  call test_sqrtm()
   call test_util()
+  call test_vector()
 
 #ifdef USE_ILUPACK
   block
