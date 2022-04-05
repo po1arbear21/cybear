3fff13ca647119dbe670e40aae0f4cbbaebc2bd1
Hendrik Leenders <hendrik.leenders@rwth-aachen.de>
2022-04-05 11:45:54 +0200

    Fermi-Dirac integral

--- options.mk
+++ options.mk
@@ -2,7 +2,7 @@
 BUILD = debug
 
 # compiler (intel or gnu)
-COMPILER = intel
+COMPILER = gnu
 
 # default integer size (32 or 64)
 INTSIZE = 32
@@ -12,8 +12,8 @@ IDXSIZE = 32
 
 # libraries (disable unused, ILUPACK and MUMPS are incompatible)
 #USE_ARPACK   = true
-USE_EXPOKIT  = true
-USE_FEAST    = true
+# USE_EXPOKIT  = true
+# USE_FEAST    = true
 #USE_ILUPACK  = true
-USE_MUMPS    = true
-USE_QUADPACK = true
+# USE_MUMPS    = true
+# USE_QUADPACK = true
