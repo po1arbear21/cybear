3ec2c135be476b85d2510e82abf7363c1464e5a9
Yujia Zhou <yujia.zhou@rwth-aachen.de>
2023-04-17 16:12:36 +0200

    feast eigenvalues

--- src/analysis/eigenvalues.f90
+++ src/analysis/eigenvalues.f90
@@ -6,7 +6,13 @@ module eigenvalues_m
   use esystem_m, only: esystem
   use lapack95,  only: ggev
   use math_m,    only: PI
-  use matrix_m,  only: sparse_real, dense_real, spbuild_real, block_real, matrix_convert
+  use matrix_m,  only: sparse_real, dense_real, spbuild_real, block_real, matrix_convert, sparse_cmplx, matrix_add
+  m4_divert(m4_ifdef({m4_feast},0,-1))
+  use feast_m,  only: feast_option, feast
+  m4_divert(0)
+  use gmres_m,  only: gmres_options, gmres
+  use matop_m,  only: single_matop_cmplx
+
 
   implicit none
 
@@ -17,30 +23,162 @@ module eigenvalues_m
     type(esystem), pointer :: sys => null()
       !! pointer to corresponding equation system
 
+    integer              :: ns
+      !! number of eigenvalues
     complex, allocatable :: s(:)
-      !! complex 'frequency': x = x_0 + Re{x_1 * exp(s*t)}
+      !! complex 'frequency': x = x_0 + Re{x_1 * exp( s*t)}
       !! s = sigma + j omega
     complex, allocatable :: x(:,:)
-      !! small-signal data (sys%n, sys%n)
+      !! small-signal data (sys%n, ns)
   contains
-    procedure :: run       => eigenvalues_run
-    procedure :: schur     => eigenvalues_schur
-    procedure :: partition => eigenvalues_partition
+    m4_divert(m4_ifdef({m4_feast},0,-1))
+    procedure :: run_feast   => eigenvalues_run_feast
+    m4_divert(0)
+    procedure :: run_dense    => eigenvalues_run_dense
+    procedure :: schur        => eigenvalues_schur
+    procedure :: partition    => eigenvalues_partition
+    procedure :: select_real  => eigenvalues_select_real
+    procedure :: select_imag  => eigenvalues_select_imag
+    procedure :: select_abs   => eigenvalues_select_abs
+    procedure :: select_phase => eigenvalues_select_phase
   end type
 
 contains
 
- subroutine eigenvalues_run(this, sys)
+m4_divert(m4_ifdef({m4_feast},0,-1))
+subroutine eigenvalues_run_feast(this, sys, M0, abcd)
+    class(eigenvalues),    intent(out)   :: this
+    type(esystem), target, intent(inout) :: sys
+      !! equation system to analyse
+    integer,               intent(in)    :: M0
+      !! overestimate of number of eigenvalues
+    complex, optional,     intent(in)    :: abcd(4)
+
+    logical                  :: precon
+    complex                  :: abcd_(4)
+    type(feast_option)       :: feast_opt
+    type(gmres_options)      :: gmres_opt
+    type(sparse_cmplx)       :: df, dfp, dft, A, Ap, B, Bp, ZeB_A, ZeB_Ap
+    type(single_matop_cmplx) :: ZeB_A_mul, ZeB_Ap_solve
+
+    abcd_(1) = cmplx(1.0)
+    abcd_(2) = cmplx(-1.0)
+    abcd_(3) = cmplx(1.0)
+    abcd_(4) = cmplx(1.0)
+
+    if (present(abcd)) abcd_ = abcd
+
+    precon = allocated(sys%dfp)
+
+    call sys%get_df(df)
+    call sys%get_dft(dft)
+
+    call matrix_add(df,  dft, A,  abcd_(1), -abcd_(2))
+    call matrix_add(df,  dft, B,  abcd_(3), -abcd_(4))
+
+    call feast_opt%init(df%nrows, M0)
+
+    if (precon) then
+      call sys%get_dfp(dfp)
+      call matrix_add(dfp, dft, Ap, abcd_(1), -abcd_(2))
+      call matrix_add(dfp, dft, Bp, abcd_(3), -abcd_(4))
+
+      gmres_opt%print_msg = .true.
+      gmres_opt%rtol = 1e-6
+
+      call ZeB_A_mul%init(ZeB_A)
+      call ZeB_Ap_solve%init(ZeB_Ap, inv=.true.)
+    end if
+
+    call feast(feast_opt, fact_Az, solve_Az, mulvec_A, this%s, evecR = this%x, mulvec_B = mulvec_B)
+    this%s  = (abcd_(4) * this%s - abcd_(2)) / (abcd_(1) - abcd_(3) * this%s)
+    this%ns = size(this%s)
+
+    if (ZeB_A%factorized) call ZeB_A%destruct()
+    if (ZeB_Ap%factorized) call ZeB_Ap%destruct()
+
+  contains
+
+    subroutine fact_Az(Ze, ctrans)
+      complex,           intent(in) :: Ze
+      logical, optional, intent(in) :: ctrans
+        !! factorize (Ze*B-A)^H. default: .false.
+
+      if (ZeB_A%factorized) call ZeB_A%destruct()
+      if (ZeB_Ap%factorized) call ZeB_Ap%destruct()
+
+      ! mat = Ze * B - A
+      call matrix_add(B,  A,  ZeB_A,  Ze, cmplx(-1.0))
+      if (precon) then
+        call matrix_add(Bp, Ap, ZeB_Ap, Ze, cmplx(-1.0))
+        call ZeB_Ap%factorize()
+      else
+        call ZeB_A%factorize()
+      end if
+    end subroutine
+
+    subroutine solve_Az(xb, ctrans)
+      !! Solve (Ze*B-A)*x=b
+      complex,           intent(inout) :: xb(:,:)
+        !! input:  b
+        !! output: x
+      logical, optional, intent(in)    :: ctrans
+        !! solve (Ze*B-A)^H*x=b. default: .false.
+
+      integer              :: i
+      complex, allocatable :: b(:,:)
+
+      b = xb
+      if (precon) then
+        do i = 1, size(xb, dim = 2)
+          call gmres(b(:,i), ZeB_A_mul, xb(:,i), precon = ZeB_Ap_solve)
+        end do
+      else
+        call ZeB_A%solve_mat(b, xb)
+      end if
+    end subroutine
+
+    subroutine mulvec_A(x, y, ctrans)
+      !! Matrix-vector multiplication: y <- A*x
+      complex,           intent(in)  :: x(:,:)
+        !! input vector x
+      complex,           intent(out) :: y(:,:)
+        !! output vector y
+      logical, optional, intent(in)  :: ctrans
+        !! y <- A^H*x. default: .false.
+
+      call A%mul_mat(x, y)
+    end subroutine
+
+    subroutine mulvec_B(x, y, ctrans)
+      !! Matrix-vector multiplication: y <- B*x
+      complex,           intent(in)  :: x(:,:)
+        !! input vector x
+      complex,           intent(out) :: y(:,:)
+        !! output vector y
+      logical, optional, intent(in)  :: ctrans
+        !! y <- B^H*x. default: .false.
+
+      call B%mul_mat(x, y)
+    end subroutine
+
+  end subroutine
+  m4_divert(0)
+
+  subroutine eigenvalues_run_dense(this, sys, schur)
     !! run eigenvalue analysis
     class(eigenvalues),    intent(out)   :: this
     type(esystem), target, intent(inout) :: sys
       !! equation system to analyse
+    logical, optional,     intent(in)    :: schur
 
     type(sparse_real)    :: df, dft
-    type(dense_real)     :: dfs, dfts
+    type(dense_real)     :: dfs, dfts, df1112
     integer, allocatable :: ipart(:)
-    real,    allocatable :: ar(:), ai(:), bb(:)
-    integer              :: i, j, n, ns
+    real,    allocatable :: ar(:), ai(:), bb(:), x1(:,:), x2(:,:)
+    integer              :: i, j, k, n
+
+    logical :: schur_
 
     this%sys => sys
 
@@ -48,8 +186,15 @@ contains
     call sys%get_df(df)
     call sys%get_dft(dft)
 
-    ! eliminate zero rows from dft
-    call this%schur(df, dft, dfs, dfts, ipart)
+    schur_ = .false.
+    if (present(schur)) schur_ = schur
+    if (schur_) then
+      ! eliminate zero rows from dft
+      call this%schur(df, dft, dfs, dfts, ipart, df1112)
+    else
+      call matrix_convert(df, dfs)
+      call matrix_convert(dft, dfts)
+    end if
 
     ! system size
     n = dfs%nrows
@@ -58,24 +203,56 @@ contains
     allocate (ar(n), source = 0.0)
     allocate (ai(n), source = 0.0)
     allocate (bb(n), source = 0.0)
+    allocate (x2(n,n), source = 0.0)
     dfts%d = - dfts%d
-    call ggev(dfs%d, dfts%d, ar, ai, bb)
+    call ggev(dfs%d, dfts%d, ar, ai, bb, x2)
 
     ! get number of s values
-    ns = count(bb /= 0.0)
+    this%ns = count(bb /= 0.0)
 
     ! construct complex eigenvalues
-    allocate (this%s(ns), source = (0.0, 0.0))
+    allocate (this%s(this%ns), source = (0.0, 0.0))
     j = 0
     do i = 1, n
-      if (bb(i) /= 0.0) then
-        j = j + 1
-        this%s(j) = cmplx(ar(i) / bb(i), ai(i) / bb(i))
-      end if
+      if (bb(i) == 0.0) cycle
+      j = j + 1
+      this%s(j) = cmplx(ar(i) / bb(i), ai(i) / bb(i))
     end do
+
+    ! eigenvectors
+    allocate (this%x(sys%n,this%ns))
+
+    if (schur_) then
+      allocate (x1(sys%n-n,n))
+      call df1112%mul_mat(-x2, x1)
+      do i = 1, sys%n
+        if (ipart(i) < 0) then
+          k = 0
+          do j = 1, n
+            if (bb(j) == 0) cycle
+            k = k + 1
+            this%x(i,k) = x1(-ipart(i),j)
+          end do
+        else
+          k = 0
+          do j = 1, n
+            if (bb(j) == 0) cycle
+            k = k + 1
+            this%x(i,k) = x2(ipart(i),j)
+          end do
+        end if
+      end do
+    else
+      k = 0
+      do j = 1, n
+        if (bb(j) == 0) cycle
+        k = k + 1
+        this%x(:,k) = x2(:,j)
+      end do
+    end if
   end subroutine
 
-  subroutine eigenvalues_schur(this, df, dft, dfs, dfts, ipart)
+  subroutine eigenvalues_schur(this, df, dft, dfs, dfts, ipart, df1112)
     !! get Schur-Complement of matrices by removing zero rows of dft
     class(eigenvalues),   intent(in)  :: this
     type(sparse_real),    intent(in)  :: df
@@ -88,13 +265,13 @@ contains
       !!! output schur complement of dft (remove zero rows of dft)
     integer, allocatable, intent(out) :: ipart(:)
       !!! output partition table
+    type(dense_real),     intent(out) :: df1112
+      !! df11 \ df12
 
-    type(sparse_real)    :: df21, df22, dft21, dft22
-    type(dense_real)     :: df1112
+    type(sparse_real)    :: df11, df12, df21, df22, dft11, dft12, dft21, dft22
     integer              :: i, j, n, m
     integer, allocatable :: rtab(:)
     logical              :: zero
-    type(sparse_real)    :: df11, df12, dft11, dft12
     type(dense_real)     :: rhs
 
     ! init partition table (ipart)
@@ -142,9 +319,11 @@ contains
 
     ! get df11 \ df12 (dense matrix)
     call df1112%init(n, ncols = m)
-    call df11%factorize()
-    call df11%solve_mat(rhs%d(:,1:j), df1112%d(:,1:j))
-    call df11%destruct()
+    if (j > 0) then
+      call df11%factorize()
+      call df11%solve_mat(rhs%d(:,1:j), df1112%d(:,1:j))
+      call df11%destruct()
+    end if
 
     ! decompress df1112
     do i = m, 1, -1
@@ -228,6 +407,42 @@ contains
     call sb12%save()
     call sb21%save()
     call sb22%save()
+  end subroutine
+
+  subroutine eigenvalues_select_real(this, is)
+    !! save real part of small-signal data (overwrite equation system variables)
+    class(eigenvalues), intent(in) :: this
+    integer,            intent(in) :: is
+      !! frequency index
+
+    call this%sys%set_x(real(this%x(:,is)))
+  end subroutine
+
+  subroutine eigenvalues_select_imag(this, is)
+    !! save imaginary part of small-signal data (overwrite equation system variables)
+    class(eigenvalues),  intent(in) :: this
+    integer,             intent(in) :: is
+      !! frequency index
+
+    call this%sys%set_x(aimag(this%x(:,is)))
+  end subroutine
+
+  subroutine eigenvalues_select_abs(this, is)
+    !! save absolute value of small-signal data (overwrite equation system variables)
+    class(eigenvalues),  intent(in) :: this
+    integer,             intent(in) :: is
+      !! frequency index
+
+    call this%sys%set_x(abs(this%x(:,is)))
+  end subroutine
+
+  subroutine eigenvalues_select_phase(this, is)
+    !! save phase of small-signal data (overwrite equation system variables)
+    class(eigenvalues),  intent(in) :: this
+    integer,             intent(in) :: is
+      !! frequency index
+
+    call this%sys%set_x(atan2(aimag(this%x(:,is)), real(this%x(:,is))))
   end subroutine
 
 end module
