d5c88d44bdcbfcb3b690cb683b41388d24354cb1
Tobias Linn <tl@ithe.rwth-aachen.de>
2024-10-11 13:10:57 +0200

    derivatives for tanh-sinh quadrature

--- src/test/modules/test_quad.f90
+++ src/test/modules/test_quad.f90
@@ -22,35 +22,48 @@ module test_quad_m
 contains
 
   subroutine test_quad()
-    real            :: I, inf
+    real            :: I, dIda, dIdb, dIdp(2), inf, dum(0)
     type(test_case) :: tc
 
     call tc%init("quad")
 
-    ! x**2
-    call quad(integrand1, 1.0, 2.0, I)
-    call tc%assert_eq(7.0 / 3.0, I, 1e-14, 1e-16, "I1")
+    ! 3 * x**2
+    call quad(integrand1, 1.0, 2.0, [3.0], I, dIda, dIdb, dIdp(1:1))
+    call tc%assert_eq(7.0, I, 1e-14, 1e-16, "I1")
+    call tc%assert_eq(-3.0, dIda, 1e-14, 1e-16, "dI1da")
+    call tc%assert_eq(12.0, dIdb, 1e-14, 1e-16, "dI1db")
+    call tc%assert_eq(7.0 / 3.0, dIdp(1), 1e-14, 1e-16, "dI1dp")
 
     ! singularity close to 1
-    call quad(integrand2, -1.0, 1.0, I)
-    call tc%assert_eq(-4.8598124043616721, I, 1e-14, 1e-16, "I2")
+    call quad(integrand2, -1.0, 1.0, [2.0, 1.015625], I, dIda, dIdb, dIdp)
+    call tc%assert_eq(-9.7196248087233442,    I, 1e-14, 1e-16, "I2")
+    call tc%assert_eq( 0.9922480620155039, dIda, 1e-14, 1e-16, "dI2da")
+    call tc%assert_eq(-128.0,              dIdb, 1e-14, 1e-16, "dI2db")
+    call tc%assert_eq([-4.8598124043616721, 127.007751937984496], dIdp, 1e-14, 1e-16, "dI2dp")
 
     ! singularity at 0
-    call quad(integrand3, 0.00390625, 10.0, I)
+    call quad(integrand3, 0.00390625, 10.0, dum, I, dIda, dIdb, dum)
     call tc%assert_eq(5.5470845327363952, I, 1e-14, 1e-16, "I3")
+    call tc%assert_eq(-255.50032552075055, dIda, 1e-14, 1e-16, "dI3da")
+    call tc%assert_eq(0.000045401991009687768, dIdb, 1e-14, 1e-16, "dI3db")
 
-    ! lower bound > upper bound
-    call quad(integrand3, 10.0, 0.00390625, I)
+    ! ! lower bound > upper bound
+    call quad(integrand3, 10.0, 0.00390625, dum, I, dIda, dIdb, dum)
     call tc%assert_eq(-5.5470845327363952, I, 1e-14, 1e-16, "I3a")
+    call tc%assert_eq(-0.000045401991009687768, dIda, 1e-14, 1e-16, "dI3da")
+    call tc%assert_eq(255.50032552075055, dIdb, 1e-14, 1e-16, "dI3db")
 
-    ! int_{0}^{inf} exp(-x) dx
+    ! int_{0.5}^{inf} 1.5 * exp(-x/4) dx
     inf = ieee_value(0.0, ieee_positive_inf)
-    call quad(integrand4, 0.0, inf, I)
-    call tc%assert_eq(1.0, I, 1e-14, 1e-16, "I4")
+    call quad(integrand4, 0.5, inf, [1.5, 0.25], I, dIda, dIdb, dIdp)
+    call tc%assert_eq(6*exp(-0.125), I, 1e-14, 1e-16, "I4")
+    call tc%assert_eq(-1.5*exp(-0.125), dIda, 1e-14, 1e-16, "dI4da")
+    call tc%assert_eq([4*exp(-0.125), -27*exp(-0.125)], dIdp, 1e-14, 1e-16, "dI4dp")
 
-    ! int_{-inf}^{inf} exp(-x^2) dx
-    call quad(integrand5, -inf, inf, I)
-    call tc%assert_eq(sqrt(PI), I, 1e-14, 1e-16, "I5")
+    ! int_{-inf}^{inf} exp(-2*x^2) dx
+    call quad(integrand5, -inf, inf, [2.0], I, dIda, dIdb, dIdp(1:1))
+    call tc%assert_eq(sqrt(PI/2), I, 1e-14, 1e-16, "I5")
+    call tc%assert_eq(-0.25*sqrt(PI/2), dIdp(1), 1e-14, 1e-16, "dI5dp")
 
     m4_divert(m4_ifdef({m4_mpfr},0,-1))
     call test_quad_mpfr(tc)
@@ -59,41 +72,70 @@ contains
     call tc%finish()
   end subroutine
 
-  subroutine integrand1(x, f)
+  subroutine integrand1(x, p, f, dfdx, dfdp)
     real, intent(in)  :: x
+    real, intent(in)  :: p(:)
     real, intent(out) :: f
+    real, intent(out) :: dfdx
+    real, intent(out) :: dfdp(:)
 
-    f = x * x
+    f       = p(1) * x * x
+    dfdx    = 2 * p(1) * x
+    dfdp(1) = x * x
   end subroutine
 
-  subroutine integrand2(x, f)
+  subroutine integrand2(x, p, f, dfdx, dfdp)
     real, intent(in)  :: x
+    real, intent(in)  :: p(:)
     real, intent(out) :: f
+    real, intent(out) :: dfdx
+    real, intent(out) :: dfdp(:)
 
-    f = 1 / (x - 1.015625)
+    f       = p(1) / (x - p(2))
+    dfdx    = - p(1) / (x - p(2))**2
+    dfdp(1) = 1 / (x - p(2))
+    dfdp(2) = p(1) / (x - p(2))**2
   end subroutine
 
-  subroutine integrand3(x, f)
+  subroutine integrand3(x, p, f, dfdx, dfdp)
     use math_m, only: expm1
 
     real, intent(in)  :: x
+    real, intent(in)  :: p(:)
     real, intent(out) :: f
+    real, intent(out) :: dfdx
+    real, intent(out) :: dfdp(:)
+
+    m4_ignore(p)
+    m4_ignore(dfdp)
 
     f = 1 / expm1(x)
+    dfdx = - exp(x) * f**2
   end subroutine
 
-  subroutine integrand4(x, f)
+  subroutine integrand4(x, p, f, dfdx, dfdp)
     real, intent(in)  :: x
+    real, intent(in)  :: p(:)
     real, intent(out) :: f
+    real, intent(out) :: dfdx
+    real, intent(out) :: dfdp(:)
 
-    f = exp(-x)
+    f       =   p(1)        * exp(-p(2) * x)
+    dfdx    = - p(1) * p(2) * exp(-p(2) * x)
+    dfdp(1) =                 exp(-p(2) * x)
+    dfdp(2) = - p(1) *    x * exp(-p(2) * x)
   end subroutine
 
-  subroutine integrand5(x, f)
+  subroutine integrand5(x, p, f, dfdx, dfdp)
     real, intent(in)  :: x
+    real, intent(in)  :: p(:)
     real, intent(out) :: f
+    real, intent(out) :: dfdx
+    real, intent(out) :: dfdp(:)
 
-    f = exp(-x**2)
+    f       =                  exp(-p(1) * x**2)
+    dfdx    = - 2 * p(1) * x * exp(-p(1) * x**2)
+    dfdp(1) = - x**2         * exp(-p(1) * x**2)
   end subroutine
 
   m4_divert(m4_ifdef({m4_mpfr},0,-1))
--- src/util/quad.f90
+++ src/util/quad.f90
@@ -18,11 +18,17 @@ module quad_m
   })
 
   interface
-    subroutine integrand(x, f)
+    subroutine integrand(x, p, f, dfdx, dfdp)
       real, intent(in)  :: x
         !! argument
+      real, intent(in)  :: p(:)
+        !! parameters
       real, intent(out) :: f
         !! output integrand
+      real, intent(out) :: dfdx
+        !! output derivative of f wrt x
+      real, intent(out) :: dfdp(:)
+        !! output derivatives of f wrt p
     end subroutine
     m4_ifdef({m4_mpfr},{
     subroutine integrand_mpfr(x, f)
@@ -35,9 +41,13 @@ module quad_m
     })
   end interface
 
+  integer, parameter :: MODE_TANH_SINH = 0
+  integer, parameter :: MODE_EXP_SINH  = 1
+  integer, parameter :: MODE_SINH_SINH = 2
+
 contains
 
-  subroutine quad(func, a, b, I, n, eps, e, num)
+  subroutine quad(func, a, b, p, I, dIda, dIdb, dIdp, eps, err, nlevels, ncalls)
     !! general purpose integration routine using tanh-sinh (finite interval), exp-sinh (one integration bound is infinite)
     !! or sinh-sinh (both integration bounds are infinite) quadrature with double precision (see qthsh)
     procedure(integrand)           :: func
@@ -46,145 +56,209 @@ contains
       !! lower integration bound
     real,              intent(in)  :: b
       !! upper integration bound
+    real,              intent(in)  :: p(:)
+      !! additional parameters passed to the integrand
     real,              intent(out) :: I
       !! output value of integral
-    integer, optional, intent(in)  :: n
-      !! maximal number of levels (default: 8)
+    real,              intent(out) :: dIda
+      !! output derivative of I wrt lower bound
+    real,              intent(out) :: dIdb
+      !! output derivative of I wrt upper bound
+    real,              intent(out) :: dIdp(:)
+      !! output derivatives of I wrt parameters
     real,    optional, intent(in)  :: eps
       !! error tolerance (default: 1e-14)
-    real,    optional, intent(out) :: e
+    real,    optional, intent(out) :: err
       !! output error estimate
-    integer, optional, intent(out) :: num
+    integer, optional, intent(in)  :: nlevels
+      !! maximal number of levels (default: 8)
+    integer, optional, intent(out) :: ncalls
       !! output number of function calls
 
-    integer :: k, mode, sign, n_, num_
-    real    :: a_, b_, c, d, eh, eps_, fm, fp, h, p, q, r, s, t, u, tol, v, w, x, y
+    integer :: lvl, mode, sgn, nlevels_, ncalls_
+    real    :: bnd(2), c, d, eh, eps_, f, fm, fp, h, pp, qq, r, s, t, u, tol, v, w, x
+    real    :: dfdp(size(p)), dfpdp(size(p)), dfmdp(size(p)), dppdp(size(p)), dqqdp(size(p)), dsdp(size(p))
+    real    :: dxdbnd(2), dfdx, dfpdbnd(2), dfmdbnd(2), dppdbnd(2), dqqdbnd(2), dsdbnd(2), dIdbnd(2)
 
-    a_   = a
-    b_   = b
-    n_   = 8
-    num_ = 0
-    if (present(n)) n_ = n
+    bnd      = [a, b]
+    nlevels_ = 8
+    ncalls_  = 0
+    if (present(nlevels)) nlevels_ = nlevels
     eps_ = 1e-14
     if (present(eps)) eps_ = eps
 
-    tol  = 10 * eps_
-    c    = 0
-    d    = 1
-    sign = 1
-    h    = 2
-    k    = 0
+    tol = 10 * eps_
+    sgn = 1
+    h   = 2
+    lvl = 0
 
     ! swap bounds if necessary
-    if (b_ < a_) then
-      v    = b_
-      b_   = a_
-      a_   = v
-      sign = -1
+    if (bnd(2) < bnd(1)) then
+      v      = bnd(2)
+      bnd(2) = bnd(1)
+      bnd(1) = v
+      sgn    = -1
     end if
 
     ! determine mode
-    if (ieee_is_finite(a_) .and. ieee_is_finite(b_)) then
-      mode = 0 ! tanh-sinh
-      c    = 0.5 * (a_ + b_)
-      d    = 0.5 * (b_ - a_)
-      v    = c
-    elseif (ieee_is_finite(a_)) then
-      mode = 1 ! exp-sinh
-      c    = a_
-      v    = a_ + d
-    elseif (ieee_is_finite(b_)) then
-      mode = 1 ! exp-sinh
-      d    = -d
-      sign = -sign
-      c    = b_
-      v    = b_ + d
+    if (ieee_is_finite(bnd(1)) .and. ieee_is_finite(bnd(2))) then
+      mode   = MODE_TANH_SINH
+      c      = 0.5 * (bnd(1) + bnd(2))
+      d      = 0.5 * (bnd(2) - bnd(1))
+      x      = c
+      dxdbnd = [0.5, 0.5]
+    elseif (ieee_is_finite(bnd(1))) then
+      mode   = MODE_EXP_SINH
+      c      = bnd(1)
+      d      = 1
+      x      = c + d
+      dxdbnd = [1.0, 0.0]
+    elseif (ieee_is_finite(bnd(2))) then
+      mode   = MODE_EXP_SINH
+      c      = bnd(2)
+      d      = -1
+      sgn    = -sgn
+      x      = c + d
+      dxdbnd = [0.0, 1.0]
     else
-      mode = 2 ! sinh-sinh
-      v    = 0
+      mode   = MODE_SINH_SINH
+      c      = 0
+      d      = 1
+      x      = 0
+      dxdbnd = 0
     end if
 
     ! start sum with first point
-    call func(v, s)
-    num_ = num_ + 1
+    call func(x, p, f, dfdx, dfdp)
+    ncalls_ = ncalls_ + 1
+    s       = f
+    dsdp    = dfdp
+    dsdbnd  = dfdx * dxdbnd
 
     do
-      p  = 0
-      fp = 0
-      fm = 0
-      h  = h / 2
-      eh = exp(h)
-      t  = eh
-      if (k > 0) eh = eh * eh
+      pp      = 0
+      dppdp   = 0
+      dppdbnd = 0
+      fp      = 0
+      dfpdp   = 0
+      dfpdbnd = 0
+      fm      = 0
+      dfmdp   = 0
+      dfmdbnd = 0
+      h       = h / 2
+      eh      = exp(h)
+      t       = eh
+      if (lvl > 0) eh = eh * eh
 
-      if (mode == 0) then ! tanh-sinh
+      if (mode == MODE_TANH_SINH) then ! tanh-sinh
         do
-          u = exp(1 / t - t)            ! = exp(-2*sinh(j*h)) = 1 / exp(sinh(j*h))**2
-          r = 2 * u / (1 + u)           ! = 1 - tanh(sinh(j*h))
-          w = (t + 1 / t) * r / (1 + u) ! = cosh(j*h) / cosh(sinh(j*h))**2
-          x = d * r
+          u      = exp(1 / t - t)            ! = exp(-2*sinh(j*h)) = 1 / exp(sinh(j*h))**2
+          r      = 2 * u / (1 + u)           ! = 1 - tanh(sinh(j*h))
+          w      = (t + 1 / t) * r / (1 + u) ! = cosh(j*h) / cosh(sinh(j*h))**2
+          x      = d * r
+          dxdbnd = r * [-0.5, 0.5]
 
           ! only recalculate integrand if not too close to previous value
-          if (a_ + x > a_) then
-            call func(a_ + x, y)
-            num_ = num_ + 1
-            if (ieee_is_finite(y)) fp = y
+          if (bnd(1) + x > bnd(1)) then
+            call func(bnd(1) + x, p, f, dfdx, dfdp)
+            ncalls_ = ncalls_ + 1
+            if (ieee_is_finite(f)) then
+              fp      = f
+              dfpdp   = dfdp
+              dfpdbnd = dfdx * ([1.0, 0.0] + dxdbnd)
+            end if
           end if
-          if (b_ - x < b_) then
-            call func(b_ - x, y)
-            num_ = num_ + 1
-            if (ieee_is_finite(y)) fm = y
+          if (bnd(2) - x < bnd(2)) then
+            call func(bnd(2) - x, p, f, dfdx, dfdp)
+            ncalls_ = ncalls_ + 1
+            if (ieee_is_finite(f)) then
+              fm      = f
+              dfmdp   = dfdp
+              dfmdbnd = dfdx * ([0.0, 1.0] - dxdbnd)
+            end if
           end if
 
-          q = w * (fp + fm)
-          p = p + q
-          t = t * eh
-          if (abs(q) <= eps_ * abs(p)) exit
+          qq      = w * (fp + fm)
+          dqqdp   = w * (dfpdp + dfmdp)
+          dqqdbnd = w * (dfpdbnd + dfmdbnd)
+          pp      = pp + qq
+          dppdp   = dppdp + dqqdp
+          dppdbnd = dppdbnd + dqqdbnd
+          t       = t * eh
+          if (abs(qq) <= eps_ * abs(pp)) exit
         end do
       else
         t = t / 2
         do
-          r = exp(t - 0.25 / t) ! = exp(sinh(j*h))
-          w = r
-          q = 0
-          if (mode == 1) then
-            ! exp-sinh
+          r       = exp(t - 0.25 / t) ! = exp(sinh(j*h))
+          w       = r
+          qq      = 0
+          dqqdp   = 0
+          dqqdbnd = 0
+          if (mode == MODE_EXP_SINH) then
             x = c + d / r
             if (x == c) exit
-            call func(x, y)
-            num_ = num_ + 1
-            if (ieee_is_finite(y)) q = q + y / w
-          else
-            ! sinh-sinh
+            call func(x, p, f, dfdx, dfdp)
+            ncalls_ = ncalls_ + 1
+            if (ieee_is_finite(f)) then
+              qq      = qq + f / w
+              dqqdp   = dqqdp + dfdp / w
+              dqqdbnd = dqqdbnd + dfdx * dxdbnd / w
+            end if
+          else ! MODE_SINH_SINH
             r = 0.5 * (r - 1 / r) ! = sinh(sinh(j*h))
             w = 0.5 * (w + 1 / w) ! = cosh(sinh(j*h))
             x = c - d * r
-            call func(x, y)
-            if (ieee_is_finite(y)) q = q + y * w
+            call func(x, p, f, dfdx, dfdp)
+            if (ieee_is_finite(f)) then
+              qq    = qq + f * w
+              dqqdp = dqqdp + dfdp * w
+            end if
           end if
           x = c + d * r
-          call func(x, y)
-          num_ = num_ + 1
-          if (ieee_is_finite(y)) q = q + y * w
-          q = q * (t + 0.25 / t) ! q = q * cosh(j*h)
-          p = p + q
-          t = t * eh
-          if (abs(q) <= eps_ * abs(p)) exit
+          call func(x, p, f, dfdx, dfdp)
+          ncalls_ = ncalls_ + 1
+          if (ieee_is_finite(f)) then
+            qq      = qq + f * w
+            dqqdp   = dqqdp + dfdp * w
+            dqqdbnd = dqqdbnd + dfdx * dxdbnd * w
+          end if
+          qq      = qq * (t + 0.25 / t) ! qq = qq * cosh(j*h)
+          dqqdp   = dqqdp * (t + 0.25 / t)
+          dqqdbnd = dqqdbnd * (t + 0.25 / t)
+          pp      = pp + qq
+          dppdp   = dppdp + dqqdp
+          dppdbnd = dppdbnd + dqqdbnd
+          t       = t * eh
+          if (abs(qq) <= eps_ * abs(pp)) exit
         end do
       end if
-      v = s - p
-      s = s + p
-      k = k + 1
-      if ((abs(v) <= tol * abs(s)) .or. (k > n_)) exit
+      v      = s - pp
+      s      = s + pp
+      dsdp   = dsdp + dppdp
+      dsdbnd = dsdbnd + dppdbnd
+      lvl = lvl + 1
+      if ((abs(v) <= tol * abs(s)) .or. (lvl > nlevels_)) exit
     end do
-    I = sign * d * s * h
-    if (present(e)) e = abs(v) / (abs(s) + eps_)
-    if (present(num)) num = num_
+    I      = sgn * d * s * h
+    dIdp   = sgn * d * dsdp * h
+    dIdbnd = sgn * d * dsdbnd * h
+    if (mode == MODE_TANH_SINH) then
+      dIdbnd = dIdbnd + sgn * [-0.5, 0.5] * s * h
+    end if
+    if (sgn == 1) then
+      dIda = dIdbnd(1)
+      dIdb = dIdbnd(2)
+    else
+      dIda = dIdbnd(2)
+      dIdb = dIdbnd(1)
+    end if
+    if (present(err)) err = abs(v) / (abs(s) + eps_)
+    if (present(ncalls)) ncalls = ncalls_
   end subroutine
 
   m4_divert(m4_ifdef({m4_mpfr},0,-1))
-
-  subroutine quad_mpfr(func, a, b, I, n, eps, e, num)
+  subroutine quad_mpfr(func, a, b, I, eps, err, nlevels, ncalls)
     !! general purpose integration routine using tanh-sinh (finite interval), exp-sinh (one integration bound is infinite)
     !! or sinh-sinh (both integration bounds are infinite) quadrature with multiprecision
     procedure(integrand_mpfr)           :: func
@@ -195,24 +269,24 @@ contains
       !! upper integration bound
     type(mpfr),           intent(inout) :: I
       !! output integral value (must be already initialized)
-    type(mpfr), optional, intent(inout) :: e
-      !! output error (must be already initialized)
-    integer,    optional, intent(in)    :: n
-      !! maximal number of levels (default: 16)
     real,       optional, intent(in)    :: eps
       !! error tolerance (default: 1e-32)
-    integer,    optional, intent(out)   :: num
+    type(mpfr), optional, intent(inout) :: err
+      !! output error (must be already initialized)
+    integer,    optional, intent(in)    :: nlevels
+      !! maximal number of levels (default: 16)
+    integer,    optional, intent(out)   :: ncalls
       !! output number of function calls
 
-    integer    :: k, mode, sign, n_, num_
+    integer    :: lvl, mode, sgn, nlevels_, ncalls_
     real       :: a_, b_, tmp, tol, eps_
     type(mpfr) :: c, d, eh, fm, fp, h, p, q, r, s, t, t1, t2, u, v, w, x, y
 
     a_   = a
     b_   = b
-    n_   = 16
-    num_ = 0
-    if (present(n)) n_ = n
+    nlevels_   = 16
+    ncalls_ = 0
+    if (present(nlevels)) nlevels_ = nlevels
     eps_ = 1e-32
     if (present(eps)) eps_ = eps
 
@@ -236,24 +310,24 @@ contains
     call x%init()
     call y%init()
 
-    tol  = 10 * eps_
+    tol = 10 * eps_
     call c%set(0)
     call d%set(1)
-    sign = 1
+    sgn = 1
     call h%set(2)
-    k    = 0
+    lvl = 0
 
     ! swap bounds if necessary
     if (b_ < a_) then
-      tmp  = b_
-      b_   = a_
-      a_   = tmp
-      sign = -1
+      tmp = b_
+      b_  = a_
+      a_  = tmp
+      sgn = -1
     end if
 
     ! determine mode
     if (ieee_is_finite(a_) .and. ieee_is_finite(b_)) then
-      mode = 0 ! tanh-sinh
+      mode = MODE_TANH_SINH
       call c%set(a_)       ! c = a_
       call add(c, c, b_)   ! c = a_ + b_
       call mul(c, c, 0.5)  ! c = 0.5 * (a_ + b_)
@@ -262,34 +336,34 @@ contains
       call mul(d, d, 0.5)  ! d = 0.5 * (b_ - a_)
       call v%set(c)        ! v = c
     elseif (ieee_is_finite(a_)) then
-      mode = 1 ! exp-sinh
+      mode = MODE_EXP_SINH
       call c%set(a_)       ! c = a_
       call add(v, c, d)    ! v = a_ + d
     elseif (ieee_is_finite(b_)) then
-      mode = 1 ! exp-sinh
+      mode = MODE_EXP_SINH
       call neg(d, d)       ! d = - d
-      sign = -sign
+      sgn  = -sgn
       call c%set(b_)       ! c = b_
       call add(v, c, d)    ! v = b_ + d
     else
-      mode = 2 ! sinh-sinh
+      mode = MODE_SINH_SINH
       call v%set(0)        ! v = 0
     end if
 
     ! start sum with first point
     call func(v, s)
-    num_ = num_ + 1
+    ncalls_ = ncalls_ + 1
 
     do
-      call p%set(0)               ! p  = 0
-      call fp%set(0)              ! fp = 0
-      call fm%set(0)              ! fm = 0
-      call div(h, h, 2)           ! h  = h / 2
-      call exp(eh, h)             ! eh = exp(h)
-      call t%set(eh)              ! t  = eh
-      if (k > 0) call sqr(eh, eh) ! eh = eh * eh
+      call p%set(0)     ! p  = 0
+      call fp%set(0)    ! fp = 0
+      call fm%set(0)    ! fm = 0
+      call div(h, h, 2) ! h  = h / 2
+      call exp(eh, h)   ! eh = exp(h)
+      call t%set(eh)    ! t  = eh
+      if (lvl > 0) call sqr(eh, eh) ! eh = eh * eh
 
-      if (mode == 0) then ! tanh-sinh
+      if (mode == MODE_TANH_SINH) then
         do
           call div(t1, 1, t)  ! t1 = 1 / t
           call sub(u, t1, t)  ! u = 1 / t - t
@@ -309,12 +383,12 @@ contains
           call add(t1, x, a_) ! t1 = a_ + x
           if (t1 > a_) then
             call func(t1, fp)
-            num_ = num_ + 1
+            ncalls_ = ncalls_ + 1
           end if
           call sub(t1, b_, x) ! t1 = b_ - x
           if (t1 < b_) then
             call func(t1, fm)
-            num_ = num_ + 1
+            ncalls_ = ncalls_ + 1
           end if
 
           call add(q, fp, fm)    ! q = fp + fm
@@ -335,17 +409,15 @@ contains
           call exp(r, r)       ! r = exp(t - 0.25 / t) = exp(sinh(j*h))
           call w%set(r)        ! w = r
           call q%set(0)        ! q = 0
-          if (mode == 1) then
-            ! exp-sinh
+          if (mode == MODE_EXP_SINH) then
             call div(x, d, r)  ! x = d / r
             call add(x, c, x)  ! x = c + d / r
             if (x == c) exit
             call func(x, y)
-            num_ = num_ + 1
+            ncalls_ = ncalls_ + 1
             call div(y, y, w)  ! y = y / w
             call add(q, q, y)  ! q = q + y / w
-          else
-            ! sinh-sinh
+          else ! MODE_SINH_SINH
             call div(t1, 1, r)   ! t1 = 1 / r
             call sub(t1, r, t1)  ! t1 = r - 1 / r
             call mul(r, t1, 0.5) ! r = 0.5 * (r - 1 / r) = sinh(sinh(j*h))
@@ -355,12 +427,12 @@ contains
             call fms(x, d, r, c) ! x = d * r - c
             call neg(x, x)       ! x = c - d * r
             call func(x, y)
-            num_ = num_ + 1
+            ncalls_ = ncalls_ + 1
             call fma(q, y, w, q) ! q = q + y * w
           end if
           call fma(x, d, r, c) ! x = c + d * r
           call func(x, y)
-          num_ = num_ + 1
+          ncalls_ = ncalls_ + 1
           call fma(q, y, w, q) ! q = q + y * w
 
           call div(t1, 0.25, t)  ! t1 = 0.25 / t
@@ -379,8 +451,8 @@ contains
       call sub(v, s, p) ! v = s - p
       call add(s, s, p) ! s = s + p
 
-      k = k + 1
-      if (k > n_) exit
+      lvl = lvl + 1
+      if (lvl > nlevels_) exit
 
       call abs(t1, v)       ! t1 = abs(v)
       call abs(t2, s)       ! t2 = abs(s)
@@ -389,19 +461,19 @@ contains
     end do
 
     ! integral
-    call mul(I, d, sign)     ! I = sign * d
-    call mul(I, I, s)        ! I = sign * d * s
-    call mul(I, I, h)        ! I = sign * d * s * h
+    call mul(I, d, sgn)      ! I = sgn * d
+    call mul(I, I, s)        ! I = sgn * d * s
+    call mul(I, I, h)        ! I = sgn * d * s * h
 
     ! error estimate
-    if (present(e)) then
+    if (present(err)) then
       call abs(t1, v)        ! t1 = abs(v)
       call abs(t2, s)        ! t2 = abs(s)
       call add(t2, t2, eps_) ! t2 = abs(s) + eps_
-      call div(e, t1, t2)    ! e = abs(v) / (abs(s) + eps_)
+      call div(err, t1, t2)  ! err = abs(v) / (abs(s) + eps_)
     end if
 
-    if (present(num)) num = num_
+    if (present(ncalls)) ncalls = ncalls_
 
     ! cleanup
     call c%destruct()
