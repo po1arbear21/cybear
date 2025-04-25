920145170b5a1e335e450340d5738dc995474797
Faisst <faisst@ip170.ad.ithe.rwth-aachen.de>
2025-04-25 15:58:45 +0200

    inclueded option to chose current unit when initializing a new curcuit

--- src/circuit/current.f90
+++ src/circuit/current.f90
@@ -18,17 +18,23 @@ module current_m
 
 contains
 
-  subroutine current_init(this, name)
+  subroutine current_init(this, name, unit)
     !! initialize current variable
-    class(current), intent(out) :: this
-    character(*),   intent(in)  :: name
+    class(current),         intent(out) :: this
+    character(*),           intent(in)  :: name
       !! variable name
+    character(*), optional, intent(in)  :: unit
+      !! current unit
 
     type(grid_data0_real), pointer :: p
 
     ! init base
-    call this%variable_init(name, "A")
-
+    if(present(unit)) then
+      call this%variable_init(name, unit)
+    else 
+      call this%variable_init(name, "A")
+    end if 
+    
     ! set pointer to value
     p      => this%data%get_ptr0()
     this%x => p%data
