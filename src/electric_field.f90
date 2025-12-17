585b647bbb940f5be6213321c7e93d59203a116e
Chenyang Yu <cy@ithe.rwth-aachen.de>
2025-12-17 16:00:38 +0100

    efield test

---
+++ src/electric_field.f90
@@ -0,0 +1,226 @@
+m4_include(util/macro.f90.inc)
+
+module electric_field_m
+  !! Electric field calculation module
+  !!
+  !! Calculates electric field components from potential gradient using
+  !! finite volume method. The electric field at each vertex is computed as:
+  !!
+  !!   E_k,dir = -sum_l [A_kl * (r_l - r_k)_dir / (Omega_k * L_kl)] * (phi_l - phi_k)
+  !!
+  !! where:
+  !!   A_kl    = adjoint surface area between vertices k and l
+  !!   Omega_k = control volume at vertex k
+  !!   L_kl    = edge length between vertices k and l
+  !!   r       = vertex coordinates
+  !!   phi     = electrostatic potential
+
+  use device_params_m,  only: device_params
+  use equation_m,       only: equation
+  use error_m,          only: assert_failed, program_error
+  use grid_m,           only: IDX_VERTEX, IDX_EDGE
+  use grid_data_m,      only: grid_data1_real, grid_data2_real, grid_data3_real
+  use grid_generator_m, only: DIR_NAME
+  use jacobian_m,       only: jacobian
+  use potential_m,      only: potential
+  use stencil_m,        only: near_neighb_stencil
+  use variable_m,       only: variable
+  use vselector_m,      only: vselector
+  use normalization_m,  only: denorm
+
+  implicit none
+
+  private
+  public electric_field, calc_efield
+
+  type, extends(variable) :: electric_field
+    !! electric field component at vertices
+
+    integer :: dir
+      !! field component direction (1=x, 2=y, 3=z)
+
+    real, pointer :: x1(:)     => null()
+      !! direct pointer to data for easy access (only used if idx_dim == 1)
+    real, pointer :: x2(:,:)   => null()
+      !! direct pointer to data for easy access (only used if idx_dim == 2)
+    real, pointer :: x3(:,:,:) => null()
+      !! direct pointer to data for easy access (only used if idx_dim == 3)
+  contains
+    procedure :: init => electric_field_init
+  end type
+
+  type, extends(equation) :: calc_efield
+    !! calculate electric field component from potential gradient using finite volume method
+
+    type(device_params), pointer :: par => null()
+      !! pointer to device parameters
+
+    type(vselector) :: pot
+      !! variable selector for potential in equation order
+    type(vselector) :: efield
+      !! variable selector for electric field in equation order
+
+    type(near_neighb_stencil) :: st_nn
+      !! near neighbor stencil for vertex-to-vertex connectivity
+
+    type(jacobian), pointer :: jaco_pot => null()
+      !! jacobian for potential dependency
+
+    real, allocatable :: work(:)
+      !! pre-allocated work array for evaluation (optimization: avoids repeated allocation)
+  contains
+    procedure :: init => calc_efield_init
+    procedure :: eval => calc_efield_eval
+  end type
+
+contains
+
+  subroutine electric_field_init(this, par, dir)
+    !! initialize electric field component
+    class(electric_field), intent(out) :: this
+    type(device_params),   intent(in)  :: par
+      !! device parameters
+    integer,               intent(in)  :: dir
+      !! field component direction (1=x, 2=y, 3=z)
+
+    type(grid_data1_real), pointer :: p1
+    type(grid_data2_real), pointer :: p2
+    type(grid_data3_real), pointer :: p3
+
+    ! init base variable
+    call this%variable_init("E"//DIR_NAME(dir), "V/cm", g = par%g, idx_type = IDX_VERTEX, idx_dir = 0)
+    this%dir = dir
+
+    ! get pointer to data for direct access
+    select case (par%g%idx_dim)
+    case (1)
+      p1 => this%data%get_ptr1()
+      this%x1 => p1%data
+    case (2)
+      p2 => this%data%get_ptr2()
+      this%x2 => p2%data
+    case (3)
+      p3 => this%data%get_ptr3()
+      this%x3 => p3%data
+    case default
+      call program_error("Maximal 3 dimensions allowed")
+    end select
+  end subroutine
+
+  subroutine calc_efield_init(this, par, pot, efield)
+    !! initialize electric field calculation equation
+    !!
+    !! Sets up the Jacobian matrix with pre-computed geometric factors.
+    !! The Jacobian is constant (geometry-dependent only), so it is marked
+    !! as const=.true. for efficiency.
+    class(calc_efield),   intent(out)        :: this
+    type(device_params),  intent(in), target :: par
+      !! device parameters
+    type(potential),      intent(in), target :: pot
+      !! potential variable
+    type(electric_field), intent(in), target :: efield
+      !! electric field component variable
+
+    integer :: iprov, ict, i, j, idx_dir, dir, max_neighb
+    real    :: geometric_factor, A_kl, L_kl, Omega_k
+    real    :: p_k(par%g%dim), p_l(par%g%dim)
+    integer, allocatable :: idx_k(:), idx_l(:), edge_idx(:)
+    logical :: status
+
+    print "(A)", "calc_efield_init for component "//DIR_NAME(efield%dir)
+
+    ! init base equation
+    call this%equation_init("calc_E"//DIR_NAME(efield%dir))
+    this%par => par
+
+    ! init nearest-neighbor stencil for vertex-to-vertex connectivity
+    call this%st_nn%init(par%g, IDX_VERTEX, 0, IDX_VERTEX, 0)
+
+    ! create variable selectors for equation order (interior + all contacts)
+    call this%pot%init(pot, [(par%transport_vct(ict)%get_ptr(), ict = 0, par%nct)])
+    call this%efield%init(efield, [(par%transport_vct(ict)%get_ptr(), ict = 0, par%nct)])
+
+    ! provide electric field component at all vertices (interior + contacts)
+    iprov = this%provide(efield, [(par%transport_vct(ict)%get_ptr(), ict = 0, par%nct)])
+
+    ! depend on potential at all vertices with near-neighbor stencil
+    ! const=.true. because geometric factors are fixed
+    this%jaco_pot => this%init_jaco(iprov, &
+      & this%depend(pot, [(par%transport_vct(ict)%get_ptr(), ict = 0, par%nct)]), &
+      & st = [(this%st_nn%get_ptr(), ict = 0, par%nct)], const = .true.)
+
+    ! pre-allocate work array for evaluation
+    allocate(this%work(this%efield%n))
+
+    ! Set up geometric factors in Jacobian
+    dir = efield%dir
+    allocate(idx_k(par%g%idx_dim), idx_l(par%g%idx_dim), edge_idx(par%g%idx_dim))
+
+    ! loop over all transport vertices (interior + contacts)
+    do ict = 0, par%nct
+      do i = 1, par%transport_vct(ict)%n
+        idx_k = par%transport_vct(ict)%get_idx(i)
+
+        ! get control volume at vertex k
+        Omega_k = par%tr_vol%get(idx_k)
+        if (Omega_k <= 0.0) cycle  ! skip if zero volume
+
+        ! get coordinates of vertex k
+        call par%g%get_vertex(idx_k, p_k)
+
+        ! loop over all edge directions
+        do idx_dir = 1, par%g%idx_dim
+          max_neighb = par%g%get_max_neighb(IDX_VERTEX, 0, IDX_EDGE, idx_dir)
+
+          ! loop over neighboring edges in this direction
+          do j = 1, max_neighb
+            ! get edge index
+            call par%g%get_neighb(IDX_VERTEX, 0, IDX_EDGE, idx_dir, idx_k, j, edge_idx, status)
+            if (.not. status) cycle
+
+            ! check if edge is in transport region
+            if (.not. par%transport(IDX_EDGE, idx_dir)%flags%get(edge_idx)) cycle
+
+            ! get the other vertex of this edge
+            call par%g%get_neighb(IDX_EDGE, idx_dir, IDX_VERTEX, 0, edge_idx, 1, idx_l, status)
+            if (all(idx_l == idx_k)) then
+              call par%g%get_neighb(IDX_EDGE, idx_dir, IDX_VERTEX, 0, edge_idx, 2, idx_l, status)
+            end if
+
+            ! get edge properties (use transport surface)
+            A_kl = par%tr_surf(idx_dir)%get(edge_idx)
+            L_kl = par%g%get_len(edge_idx, idx_dir)
+
+            ! get coordinates of vertex l
+            call par%g%get_vertex(idx_l, p_l)
+
+            ! calculate geometric factor for finite volume E-field:
+            ! E_k,dir = -sum_l [A_kl * (r_l - r_k)_dir / (Omega_k * L_kl)] * (phi_l - phi_k)
+            ! Factor of 0.5 accounts for contribution from both sides of control volume
+            geometric_factor = -0.5 * A_kl * (p_l(dir) - p_k(dir)) / (Omega_k * L_kl)
+
+            ! accumulate Jacobian entries: dE_k/dphi_l and dE_k/dphi_k
+            call this%jaco_pot%add(idx_k, idx_l, geometric_factor)
+            call this%jaco_pot%add(idx_k, idx_k, -geometric_factor)
+          end do
+        end do
+      end do
+    end do
+
+    ! finish initialization (sets constant parts of Jacobian matrix)
+    call this%init_final()
+  end subroutine
+
+  subroutine calc_efield_eval(this)
+    !! evaluate electric field calculation equation
+    !!
+    !! Computes E = J * phi where J is the pre-computed Jacobian matrix
+    !! containing the geometric factors.
+    class(calc_efield), intent(inout) :: this
+
+    ! matrix-vector multiply: E = J * phi
+    call this%jaco_pot%matr%mul_vec(this%pot%get(), this%work)
+    call this%efield%set(this%work)
+  end subroutine
+
+end module
